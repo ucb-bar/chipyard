8505078c411d86d8d0e0cd178d5b9b0e68b53aa5
Howard Mao <zhehao.mao@gmail.com>
2017-06-23 13:12:57 -0700

    fixup licensing and attribution

--- tests/rocc.h
+++ tests/rocc.h
@@ -1,29 +1,5 @@
-// Copyright (c) 2016, Boston University (BU). All Rights Reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the
-//    distribution.
-// 3. Neither the name of BU nor the names of its contributors may be
-//    used to endorse or promote products derived from this software
-//    without specific prior written permission.
-//
-// IN NO EVENT SHALL BU BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
-// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
-// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
-// BU HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// BU SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED
-// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-// PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF
-// ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". BU HAS NO OBLIGATION TO
-// PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+// Based on code by Schuyler Eldridge. Copyright (c) Boston University
+// https://github.com/seldridge/rocket-rocc-examples/blob/master/src/main/c/rocc.h
 
 #ifndef SRC_MAIN_C_ROCC_H
 #define SRC_MAIN_C_ROCC_H
