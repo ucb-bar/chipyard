119e563ea60353d266da180f259d05cec079c868
Howard Mao <zhehao.mao@gmail.com>
2017-06-22 17:41:48 -0700

    fix verilator build

--- src/main/scala/example/Configs.scala
+++ src/main/scala/example/Configs.scala
@@ -3,9 +3,12 @@ package example
 import config.{Parameters, Config}
 import testchipip.WithSerialAdapter
 import coreplex.WithRoccExample
+import rocketchip.WithoutTLMonitors
 
 class DefaultExampleConfig extends Config(
-  new WithSerialAdapter ++ new rocketchip.DefaultConfig)
+  new WithoutTLMonitors ++
+  new WithSerialAdapter ++
+  new rocketchip.DefaultConfig)
 
 class RoccExampleConfig extends Config(
   new WithRoccExample ++ new DefaultExampleConfig)
