e09f8b1b0d70f3fc0ce75990f09d4cc8d7ed7efc
Edward Wang <edward.c.wang@compdigitec.com>
2017-10-03 01:06:02 -0700

    Fix grammar

--- macros/src/main/scala/MacroCompiler.scala
+++ macros/src/main/scala/MacroCompiler.scala
@@ -588,9 +588,9 @@ object MacroCompiler extends App {
     "  -c, --cost-func: Cost function to use. Optional (default: \"default\")",
     "  -cp, --cost-param: Cost function parameter. (Optional depending on the cost function.). e.g. -c ExternalMetric -cp path /path/to/my/cost/script",
   """  --mode:
-    |    synflops: Produces synthesizable flop-based memories (for all memories and library memory macros); likely useful for simulation purposes")
-    |    fallbacksynflops: Compile all memories to library when possible and fall back to synthesizable flop-based memories when library synth is not possible
-    |    strict: Compile all memories to library or return an error
+    |    synflops: Produces synthesizable flop-based memories (for all memories and library memory macros); likely useful for simulation purposes.
+    |    fallbacksynflops: Compile all memories to library when possible and fall back to synthesizable flop-based memories when library synth is not possible.
+    |    strict: Compile all memories to library or return an error.
     |    default: Compile all memories to library when possible and do nothing in case of errors.
   """.stripMargin) mkString "\n"
 
