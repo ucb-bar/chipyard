428ca198483ef92f96d92dd081f733a699f9e669
Howard Mao <zhehao.mao@gmail.com>
2018-02-24 22:59:13 -0800

    use DRAMSim in boomexample

--- src/main/scala/boomexample/TestHarness.scala
+++ src/main/scala/boomexample/TestHarness.scala
@@ -1,9 +1,10 @@
 package boomexample
 
 import chisel3._
+import freechips.rocketchip.coreplex.{ExtMem, CacheBlockBytes}
 import freechips.rocketchip.diplomacy.LazyModule
 import freechips.rocketchip.config.{Field, Parameters}
-import testchipip.GeneratorApp
+import testchipip.{GeneratorApp, SimDRAM}
 
 case object BuildBoomTop extends Field[(Clock, Bool, Parameters) => BoomExampleTopModule[BoomExampleTop]]
 
@@ -14,10 +15,19 @@ class TestHarness(implicit val p: Parameters) extends Module {
 
   val dut = p(BuildBoomTop)(clock, reset.toBool, p)
   dut.debug := DontCare
-  dut.connectSimAXIMem()
   dut.dontTouchPorts()
   dut.tieOffInterrupts()
   io.success := dut.connectSimSerial()
+
+  val memSize = p(ExtMem).size / dut.mem_axi4.size
+  val lineSize = p(CacheBlockBytes)
+
+  dut.mem_axi4.foreach { case mem =>
+    val sim = Module(new SimDRAM(memSize, lineSize, mem.params))
+    sim.io.axi <> mem
+    sim.io.reset := reset
+    sim.io.clock := clock
+  }
 }
 
 object Generator extends GeneratorApp {
