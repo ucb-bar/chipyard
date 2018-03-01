73d44ea955fd4d65cb64928e31818cd8b489a052
Howard Mao <zhehao.mao@gmail.com>
2018-03-01 15:10:38 -0800

    lab3 setup: BHT branch predictor and branch instruction/mispredict counters

--- src/main/scala/boomexample/Configs.scala
+++ src/main/scala/boomexample/Configs.scala
@@ -1,6 +1,7 @@
 package boomexample
 
-import boom.system.BoomConfig
+import boom._
+import boom.system.{BoomConfig, BoomTilesKey}
 import chisel3._
 import example.WithBootROM
 import example.ConfigValName._
@@ -30,11 +31,38 @@ class WithSimBlockDevice extends Config((site, here, up) => {
   }
 })
 
+class WithLab3Settings extends Config((site, here, up) => {
+  case BHTKey => BHTParameters()
+  case BoomTilesKey => up(BoomTilesKey, site).map { r => r.copy(
+    core = r.core.copy(
+      fetchWidth = 1,
+      decodeWidth = 1,
+      numRobEntries = 16,
+      issueParams = Seq(
+            IssueParams(issueWidth=1, numEntries=4, iqType=IQT_MEM.litValue),
+            IssueParams(issueWidth=1, numEntries=4, iqType=IQT_INT.litValue),
+            IssueParams(issueWidth=1, numEntries=4, iqType=IQT_FP.litValue)),
+      enableBranchPredictor = true,
+      enableBTBContainsBranches = false,
+      gshare = None,
+      numIntPhysRegisters = 64,
+      numFpPhysRegisters = 64,
+      numLsuEntries = 4,
+      maxBrCount = 4,
+      nPerfCounters = 2),
+    btb = None,
+    icache = Some(r.icache.get.copy(fetchBytes=1*4)))
+  }
+})
+
 class BaseExampleConfig extends Config(
   new WithBootROM ++ new BoomConfig)
 
 class DefaultExampleConfig extends Config(
   new WithBoomExampleTop ++ new BaseExampleConfig)
+
+class Lab3Config extends Config(
+  new WithLab3Settings ++ new DefaultExampleConfig)
 
 class SimBlockDeviceConfig extends Config(
   new WithBlockDevice ++ new WithSimBlockDevice ++ new BaseExampleConfig)
