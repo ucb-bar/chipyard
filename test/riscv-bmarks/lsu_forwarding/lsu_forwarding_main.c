20f931a54a44c63ff65d21f68760e355ad47fd7f
Howard Mao <zhehao.mao@gmail.com>
2018-03-02 09:19:49 -0800

    add lsu_forwarding and lsu_failures skeleton benchmarks

---
+++ test/riscv-bmarks/lsu_forwarding/lsu_forwarding_main.c
@@ -0,0 +1,34 @@
+// See LICENSE for license details.
+
+//**************************************************************************
+// Vector-vector add benchmark
+//--------------------------------------------------------------------------
+//
+// This benchmark uses adds to vectors and writes the results to a
+// third vector. The input data (and reference data) should be
+// generated using the vvadd_gendata.pl perl script and dumped
+// to a file named dataset1.h.
+ 
+#include "util.h"
+
+//--------------------------------------------------------------------------
+// lsu_forwarding function
+
+void lsu_forwarding( /* args */ )
+{
+  // code
+}
+
+//--------------------------------------------------------------------------
+// Main
+
+int main( int argc, char* argv[] )
+{
+  // Do the vvadd
+  setStats(1);
+  lsu_forwarding();
+  setStats(0);
+
+  // Check the results
+  return 0;
+}
