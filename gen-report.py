6d8ee9aa8271e2570df6206fe607c5b37d64c71e
Howard Mao <zhehao.mao@gmail.com>
2018-03-01 16:02:09 -0800

    change benchmark running to support report generation

---
+++ gen-report.py
@@ -0,0 +1,27 @@
+import re
+import sys
+
+ENTRY_RE = re.compile(r"(\w+) = (\d+)")
+
+def calc_stats(fname):
+    with open(fname) as f:
+        counts = {}
+        for line in f:
+            m = ENTRY_RE.match(line)
+            if m:
+                name, val = m.groups()
+                counts[name] = int(val)
+        cpi = float(counts['minstret']) / counts['mcycle']
+        pa = 1.0 - float(counts['mhpmcounter3']) / counts['mhpmcounter4']
+        return cpi, pa
+
+def main():
+    for fname in sys.argv[1:]:
+        (cpi, pa) = calc_stats(fname)
+        print("==> {} <==".format(fname))
+        print("CPI: {:0.2f}".format(cpi))
+        print("Prediction Accuracy: {:0.2f}".format(pa))
+        print("")
+
+if __name__ == "__main__":
+    main()
