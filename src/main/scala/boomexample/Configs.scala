99b3a83f83860c880e306134658020c5e0964da6
Howard Mao <zhehao.mao@gmail.com>
2018-03-02 09:35:11 -0800

    make it easier to change Lab3 settings

--- src/main/scala/boomexample/Configs.scala
+++ src/main/scala/boomexample/Configs.scala
@@ -9,6 +9,20 @@ import freechips.rocketchip.config.{Parameters, Config}
 import freechips.rocketchip.diplomacy.LazyModule
 import testchipip._
 
+object Lab3Settings {
+  val N_ROB_ENTRIES = 16
+  val PHYS_REG_COUNT = 64
+  val USE_BRANCH_PREDICTOR = true
+  val FETCH_BUF_SIZE = 8
+  val N_ISSUE_SLOTS = 4
+  val N_LSU_ENTRIES = 4
+  val N_BHT_ENTRIES = 128
+  val MAX_BR_COUNT = 4
+  val N_MSHRS = 2
+  val DCACHE_WAYS = 4
+  val DCACHE_LINES = 256
+}
+
 class WithBoomExampleTop extends Config((site, here, up) => {
   case BuildBoomTop => (clock: Clock, reset: Bool, p: Parameters) => {
     Module(LazyModule(new BoomExampleTop()(p)).module)
@@ -31,26 +45,33 @@ class WithSimBlockDevice extends Config((site, here, up) => {
   }
 })
 
+import Lab3Settings._
+
 class WithLab3Settings extends Config((site, here, up) => {
-  case BHTKey => BHTParameters()
+  case BHTKey => BHTParameters(num_entries = N_BHT_ENTRIES)
   case BoomTilesKey => up(BoomTilesKey, site).map { r => r.copy(
     core = r.core.copy(
       fetchWidth = 1,
       decodeWidth = 1,
-      numRobEntries = 16,
+      fetchBufferSz = FETCH_BUF_SIZE,
+      numRobEntries = N_ROB_ENTRIES,
       issueParams = Seq(
-            IssueParams(issueWidth=1, numEntries=4, iqType=IQT_MEM.litValue),
-            IssueParams(issueWidth=1, numEntries=4, iqType=IQT_INT.litValue),
-            IssueParams(issueWidth=1, numEntries=4, iqType=IQT_FP.litValue)),
-      enableBranchPredictor = true,
+            IssueParams(issueWidth=1, numEntries=N_ISSUE_SLOTS, iqType=IQT_MEM.litValue),
+            IssueParams(issueWidth=1, numEntries=N_ISSUE_SLOTS, iqType=IQT_INT.litValue),
+            IssueParams(issueWidth=1, numEntries=N_ISSUE_SLOTS, iqType=IQT_FP.litValue)),
+      enableBranchPredictor = USE_BRANCH_PREDICTOR,
       enableBTBContainsBranches = false,
       gshare = None,
-      numIntPhysRegisters = 64,
-      numFpPhysRegisters = 64,
-      numLsuEntries = 4,
-      maxBrCount = 4,
+      numIntPhysRegisters = PHYS_REG_COUNT,
+      numFpPhysRegisters = PHYS_REG_COUNT,
+      numLsuEntries = N_LSU_ENTRIES,
+      maxBrCount = MAX_BR_COUNT,
       nPerfCounters = 2),
     btb = None,
+    dcache = Some(r.dcache.get.copy(
+      nSets = DCACHE_LINES / DCACHE_WAYS,
+      nWays = DCACHE_WAYS,
+      nMSHRs = N_MSHRS)),
     icache = Some(r.icache.get.copy(fetchBytes=1*4)))
   }
 })
