91ea25999dfa8429cb66c394b9dd9686a58c8ca9
abejgonzalez <abe.j.gonza@gmail.com>
2019-04-24 19:08:29 -0700

    better exits

--- scripts/regression.sh
+++ scripts/regression.sh
@@ -5,18 +5,27 @@
 # Run by just giving the test to run (run-bmark-tests | run-asm-tests)
 # Runs in vsim and verisim
 
-set -xe
+set -ex
+set -euo pipefail
 
 cd sims/vsim/
 
-make SUB_PROJECT=rocketchip CONFIG=DefaultConfig      && make SUB_PROJECT=rocketchip CONFIG=DefaultConfig $1
-make SUB_PROJECT=boom CONFIG=BoomConfig               && make SUB_PROJECT=boom CONFIG=BoomConfig $1
-make SUB_PROJECT=example CONFIG=DefaultRocketConfig   && make SUB_PROJECT=example CONFIG=DefaultRocketConfig $1
-make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig && make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig $1
+make SUB_PROJECT=rocketchip CONFIG=DefaultConfig
+make SUB_PROJECT=rocketchip CONFIG=DefaultConfig $1
+make SUB_PROJECT=boom CONFIG=BoomConfig
+make SUB_PROJECT=boom CONFIG=BoomConfig $1
+make SUB_PROJECT=example CONFIG=DefaultRocketConfig
+make SUB_PROJECT=example CONFIG=DefaultRocketConfig $1
+make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig
+make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig $1
 
 cd ../verisim/
 
-make SUB_PROJECT=rocketchip CONFIG=DefaultConfig      && make SUB_PROJECT=rocketchip CONFIG=DefaultConfig $1
-make SUB_PROJECT=boom CONFIG=BoomConfig               && make SUB_PROJECT=boom CONFIG=BoomConfig $1
-make SUB_PROJECT=example CONFIG=DefaultRocketConfig   && make SUB_PROJECT=example CONFIG=DefaultRocketConfig $1
-make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig && make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig $1
+make SUB_PROJECT=rocketchip CONFIG=DefaultConfig
+make SUB_PROJECT=rocketchip CONFIG=DefaultConfig $1
+make SUB_PROJECT=boom CONFIG=BoomConfig
+make SUB_PROJECT=boom CONFIG=BoomConfig $1
+make SUB_PROJECT=example CONFIG=DefaultRocketConfig
+make SUB_PROJECT=example CONFIG=DefaultRocketConfig $1
+make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig
+make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig $1
