8d190ee7e0222505c3d5766e47df362a3286b43e
abejgonzalez <abe.j.gonza@gmail.com>
2019-04-24 18:42:12 -0700

    do subprojects first

--- scripts/regression.sh
+++ scripts/regression.sh
@@ -9,14 +9,14 @@ set -xe
 
 cd sims/vsim/
 
+make SUB_PROJECT=rocketchip CONFIG=DefaultConfig      && make SUB_PROJECT=rocketchip CONFIG=DefaultConfig $1
+make SUB_PROJECT=boom CONFIG=BoomConfig               && make SUB_PROJECT=boom CONFIG=BoomConfig $1
 make SUB_PROJECT=example CONFIG=DefaultRocketConfig   && make SUB_PROJECT=example CONFIG=DefaultRocketConfig $1
 make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig && make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig $1
-make SUB_PROJECT=boom CONFIG=BoomConfig               && make SUB_PROJECT=boom CONFIG=BoomConfig $1
-make SUB_PROJECT=rocketchip CONFIG=DefaultConfig      && make SUB_PROJECT=rocketchip CONFIG=DefaultConfig $1
 
 cd ../verisim/
 
+make SUB_PROJECT=rocketchip CONFIG=DefaultConfig      && make SUB_PROJECT=rocketchip CONFIG=DefaultConfig $1
+make SUB_PROJECT=boom CONFIG=BoomConfig               && make SUB_PROJECT=boom CONFIG=BoomConfig $1
 make SUB_PROJECT=example CONFIG=DefaultRocketConfig   && make SUB_PROJECT=example CONFIG=DefaultRocketConfig $1
 make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig && make SUB_PROJECT=boomexample CONFIG=DefaultBoomConfig $1
-make SUB_PROJECT=boom CONFIG=BoomConfig               && make SUB_PROJECT=boom CONFIG=BoomConfig $1
-make SUB_PROJECT=rocketchip CONFIG=DefaultConfig      && make SUB_PROJECT=rocketchip CONFIG=DefaultConfig $1
