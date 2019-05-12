9f7f94b6f979ecb9f39a5e796516c95c359ec0b9
Abraham Gonzalez <abe.j.gonza@gmail.com>
2019-05-11 18:35:34 -0700

    parallelize toolchain builds with verilator

--- .circleci/build-toolchains.sh
+++ .circleci/build-toolchains.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
 # create the riscv tools/esp tools binaries
+# passed in as <riscv-tools or esp-tools>
 
 # turn echo on and error on earliest command
 set -ex
 
-if [ ! -d "$HOME/esp-tools-install" ]; then
+if [ ! -d "$HOME/$1-install" ]; then
 
     cd $HOME/
 
     # init all submodules including the tools
-    REBAR_DIR=$HOME/project ./project/scripts/build-toolchains.sh esp-tools
-fi
-
-if [ ! -d "$HOME/riscv-tools-install" ]; then
-
-    cd $HOME/
-
-    # init all submodules including the tools
-    REBAR_DIR=$HOME/project ./project/scripts/build-toolchains.sh riscv-tools
+    REBAR_DIR=$HOME/project ./project/scripts/build-toolchains.sh $1
 fi
