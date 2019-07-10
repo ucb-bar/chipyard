9f9b7087ed88ee7fe1bb81243b210f3f99444a8f
abejgonzalez <abe.j.gonza@gmail.com>
2019-07-10 15:50:22 -0700

    fix key/value store access

--- .circleci/run-tests.sh
+++ .circleci/run-tests.sh
@@ -26,22 +26,22 @@ run_both () {
 
 case $1 in
     example)
-        run_bmark $mapping[$1]
+        run_bmark ${mapping[$1]}
         ;;
     boomexample)
-        run_bmark $mapping[$1]
+        run_bmark ${mapping[$1]}
         ;;
     boomrocketexample)
-        run_bmark $mapping[$1]
+        run_bmark ${mapping[$1]}
         ;;
     boom)
-        run_bmark $mapping[$1]
+        run_bmark ${mapping[$1]}
         ;;
     rocketchip)
-        run_bmark $mapping[$1]
+        run_bmark ${mapping[$1]}
         ;;
     hwacha)
-        make run-rv64uv-p-asm-tests-fst -C $LOCAL_SIM_DIR VERILATOR_INSTALL_DIR=$LOCAL_VERILATOR_DIR $mapping[$1]
+        make run-rv64uv-p-asm-tests-fst -C $LOCAL_SIM_DIR VERILATOR_INSTALL_DIR=$LOCAL_VERILATOR_DIR ${mapping[$1]}
         ;;
     *)
         echo "No set of tests for $1. Did you spell it right?"
