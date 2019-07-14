e8e509daf0d79e80ea0428b7ff7f8a8635e8f868
abejgonzalez <abe.j.gonza@gmail.com>
2019-07-14 14:07:52 -0700

    bump hwacha and esp-tools

--- scripts/regression.sh
+++ scripts/regression.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
+#################
+# TESTING SCRIPT
+#################
+
 set -ex
 
 cd sims/vsim/
 
-# no hwacha tests for right now
-echo "Make sure you are using esp-tools!!!!!"
+echo ">>>Make sure you are using RISCV=.../esp-tools<<<"
+echo ">>>>>This will break if you are using the default toolchain<<<<<"
+echo "If not already done..."
+echo "    Run \`./scripts/build-toolchain.sh esp-tools\` to build an esp-tools toolchain"
+echo "    Then run \`source env.sh\` to setup RISCV, LD_LIBRARY_PATH, PATH"
+echo "If esp-tools already built..."
+echo "    Run \`source env.sh\` with the adjusted path to your prebuilt esp-tools"
 
-BCONFIG=MegaBeagleSimConfig
+: ${BCONFIG:=MegaBeagleConfig} # default value of the config
 
-# test asm/bmark tests
+# change the config if not specified
+if [ $# -ne 0 ]; then
+  BCONFIG=$1
+fi
+
+# test asm/bmark tests (includes hwacha tests (asm/bmark))
 make CONFIG=$BCONFIG
 make CONFIG=$BCONFIG run-asm-tests
 make CONFIG=$BCONFIG run-bmark-tests
 
-# test with systolic array
+# test individual binaries with the systolic array (assumes sys. is on hart1)
 TEST_DIR=$(pwd)/../../tests/beagle-systolic-tests
 tests=("matmul" "matmul_os" "matmul_ws" "mvin_mvout" "mvin_mvout_acc" "mvin_mvout_acc_stride" "mvin_mvout_stride" "raw_hazard" "tiled_matmul_os" "tiled_matmul_ws")
 for t in "${tests[@]}"
