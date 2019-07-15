fd5a00a0ab9d2fa8635e6a9566afd6662417c2a3
abejgonzalez <abe.j.gonza@gmail.com>
2019-07-15 10:58:41 -0700

    rename sim dirs | add "fast" helper target | re-add -q flag

--- common.mk
+++ common.mk
@@ -73,10 +73,13 @@ $(HARNESS_SMEMS_FILE) $(HARNESS_SMEMS_FIR): $(HARNESS_SMEMS_CONF)
 verilog: $(sim_vsrcs)
 
 #########################################################################################
-# helper rules to run simulator
+# helper rules to run simulations
 #########################################################################################
+.PHONY: run-binary run-fast
 run-binary: $(sim)
 	(set -o pipefail && $(sim) $(PERMISSIVE_ON) $(SIM_FLAGS) $(PERMISSIVE_OFF) $(BINARY) 3>&1 1>&2 2>&3 | spike-dasm > $(sim_out_name).out)
+
+run-fast: run-asm-tests-fast run-bmark-tests-fast
 
 #########################################################################################
 # run assembly/benchmarks rules
