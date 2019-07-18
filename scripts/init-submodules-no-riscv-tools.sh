40203f60a3636e263b6dc02d48fbb9fbd920fd3b
alonamid <alonamid@eecs.berkeley.edu>
2019-07-18 04:35:31 +0000

    update deinit name

--- scripts/init-submodules-no-riscv-tools.sh
+++ scripts/init-submodules-no-riscv-tools.sh
@@ -11,14 +11,14 @@ scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # ignore riscv-tools for submodule init recursive
 # you must do this globally (otherwise riscv-tools deep
 # in the submodule tree will get pulled anyway
-git config --global submodule.riscv-tools.update none
+git config --global submodule.toolchains/riscv-tools.update none
 git config --global submodule.esp-tools.update none
 git config --global submodule.experimental-blocks.update none
 # Disable updates to the FireSim submodule until explicitly requested
 git config submodule.sims/firesim.update none
 git submodule update --init --recursive #--jobs 8
 # unignore riscv-tools,catapult-shell2 globally
-git config --global --unset submodule.riscv-tools.update
+git config --global --unset submodule.toolchains/riscv-tools.update
 git config --global --unset submodule.esp-tools.update
 git config --global --unset submodule.experimental-blocks.update
 
