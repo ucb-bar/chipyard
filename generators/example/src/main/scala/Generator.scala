216167c39823fec3c825fe1e84f632b9af01b1cb
abejgonzalez <abe.j.gonza@gmail.com>
2019-07-18 16:35:32 -0700

    add hwacha generator in example

--- generators/example/src/main/scala/Generator.scala
+++ generators/example/src/main/scala/Generator.scala
@@ -4,6 +4,7 @@ import chisel3._
 
 import freechips.rocketchip.config.{Parameters}
 import freechips.rocketchip.util.{GeneratorApp}
+import freechips.rocketchip.system.{TestGeneration}
 
 import boom.system.{BoomTilesKey, TestSuiteHelper}
 
@@ -13,6 +14,37 @@ object Generator extends GeneratorApp {
     implicit val p: Parameters = params
     TestSuiteHelper.addRocketTestSuites
     TestSuiteHelper.addBoomTestSuites
+  }
+
+  // specify the name that the generator outputs files as
+  val longName = names.topModuleProject + "." + names.topModuleClass + "." + names.configs
+
+  // generate files
+  generateFirrtl
+  generateAnno
+  generateTestSuiteMakefrags
+  generateArtefacts
+}
+
+object GeneratorWithHwacha extends GeneratorApp {
+  // add unique test suites
+  override def addTestSuites {
+    implicit val p: Parameters = params
+    TestSuiteHelper.addRocketTestSuites
+    TestSuiteHelper.addBoomTestSuites
+
+    // add hwacha bmarks + asm tests
+    import hwacha.HwachaTestSuites._
+    TestGeneration.addSuites(rv64uv.map(_("p")))
+    TestGeneration.addSuites(rv64uv.map(_("vp")))
+    TestGeneration.addSuite(rv64sv("p"))
+    TestGeneration.addSuite(hwachaBmarks)
+  }
+
+  override def generateTestSuiteMakefrags {
+    addTestSuites
+    var frag = TestGeneration.generateMakefrag + "\nSRC_EXTENSION = $(base_dir)/hwacha/$(src_path)/*.scala" + "\nDISASM_EXTENSION = --extension=hwacha"
+    writeOutputFile(td, s"$longName.d", frag)
   }
 
   // specify the name that the generator outputs files as
