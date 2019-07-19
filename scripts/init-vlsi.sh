b923617f644eed0d1777b1ffa50662f02369acba
Jerry Zhao <jerryz123@berkeley.edu>
2019-07-19 15:06:51 -0700

    Fixup init-vlsi.sh

--- scripts/init-vlsi.sh
+++ scripts/init-vlsi.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+# exit script if any command fails 
+set -e
+set -o pipefail
+
+
+
 # Initialize HAMMER and CAD-plugins
 git submodule update --init --recursive vlsi/hammer
 git submodule update --init --recursive vlsi/hammer-cad-plugins
