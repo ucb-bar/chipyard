c1004790cc6eecb1802c05421699933d8a0ce04d
Colin Schmidt <colins@eecs.berkeley.edu>
2019-10-28 07:33:04 -0700

    Use x instead of e to match other case

--- tapeout/src/main/scala/transforms/Generate.scala
+++ tapeout/src/main/scala/transforms/Generate.scala
@@ -218,7 +218,7 @@ sealed trait GenerateTopAndHarnessApp extends LazyLogging { this: App =>
       case x: FirrtlExecutionSuccess =>
         dump(x, tapeoutOptions.topFir, tapeoutOptions.topAnnoOut)
         x.circuitState.circuit.modules.collect{ case e: ExtModule => e }
-      case e =>
+      case x =>
         throw new Exception(s"executeTop failed while executing FIRRTL!\n${e}")
     }
   }
@@ -243,7 +243,7 @@ sealed trait GenerateTopAndHarnessApp extends LazyLogging { this: App =>
     val harnessResult = firrtl.Driver.execute(optionsManager)
     harnessResult match {
       case x: FirrtlExecutionSuccess => dump(x, tapeoutOptions.harnessFir, tapeoutOptions.harnessAnnoOut)
-      case e => throw new Exception(s"executeHarness failed while executing FIRRTL!\n${e}")
+      case x => throw new Exception(s"executeHarness failed while executing FIRRTL!\n${e}")
     }
   }
 }
