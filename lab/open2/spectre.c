18f57b17fe7fcd04b2c74cee79473ed6444d87e0
Albert Ou <aou@eecs.berkeley.edu>
2020-03-22 01:53:39 -0700

    cs152: Add printx() function to output results

--- lab/open2/spectre.c
+++ lab/open2/spectre.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <unistd.h>
 
 static inline void leak(size_t index, int shift)
 {
@@ -17,6 +18,19 @@ static inline unsigned long get_cycles(void)
     unsigned long cycles;
     __asm__ __volatile__ ("rdcycle %0" : "=r" (cycles));
     return cycles;
+}
+
+/* Specialized output function to avoid printf() overhead
+   Use this to emit results for the autograder, one byte per line */
+static inline void printx(unsigned char x)
+{
+    static const char hex[] = "0123456789abcdef";
+    char buf[3];
+
+    buf[0] = hex[(x >> 4) & 0xf];
+    buf[1] = hex[x & 0xf];
+    buf[2] = '\n';
+    write(STDOUT_FILENO, buf, sizeof(buf));
 }
 
 int main(void)
