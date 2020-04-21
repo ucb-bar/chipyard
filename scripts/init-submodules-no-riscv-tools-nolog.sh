855c79db5b89a0978c4853e47dbde15b75a7aa36
Albert Ou <aou@eecs.berkeley.edu>
2020-04-20 21:40:19 -0700

    cs152: Skip initializing unnecessary submodules

--- scripts/init-submodules-no-riscv-tools-nolog.sh
+++ scripts/init-submodules-no-riscv-tools-nolog.sh
@@ -29,6 +29,7 @@ git config submodule.toolchains/qemu.update none
 
 # Don't automatically initialize generators with big submodules (e.g. linux source)
 git config submodule.generators/sha3.update none
+git config submodule.generators/ariane.update none
 
 # Disable updates to the FireSim submodule until explicitly requested
 git config submodule.sims/firesim.update none
@@ -51,10 +52,12 @@ git config --unset submodule.vlsi/hammer-synopsys-plugins.update
 git config --unset submodule.vlsi/hammer-mentor-plugins.update
 
 git config --unset submodule.generators/sha3.update
+git config --unset submodule.generators/ariane.update
 git config --unset submodule.software/firemarshal.update
 
 # Non-recursive clone to exclude riscv-linux
 git submodule update --init generators/sha3
+git submodule update --init generators/ariane
 
 git config --unset submodule.sims/firesim.update
 # Minimal non-recursive clone to initialize sbt dependencies
