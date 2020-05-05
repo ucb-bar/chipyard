a255417513180ab4e4f53ee20b964e9eac701674
Colin Schmidt <colins@eecs.berkeley.edu>
2020-05-05 15:24:51 -0700

    Update stage to use Dependency instead of classof

--- generators/chipyard/src/main/scala/stage/ChipyardStage.scala
+++ generators/chipyard/src/main/scala/stage/ChipyardStage.scala
@@ -10,27 +10,27 @@ import firrtl.stage.FirrtlCli
 import freechips.rocketchip.stage.RocketChipCli
 import freechips.rocketchip.system.RocketChipStage
 
-import firrtl.options.{Phase, PhaseManager, PreservesAll, Shell, Stage, StageError, StageMain}
+import firrtl.options.{Phase, PhaseManager, PreservesAll, Shell, Stage, StageError, StageMain, Dependency}
 import firrtl.options.phases.DeletedWrapper
 
 class ChipyardStage extends ChiselStage with PreservesAll[Phase] {
   override val shell = new Shell("chipyard") with ChipyardCli with RocketChipCli with ChiselCli with FirrtlCli
   override val targets: Seq[PhaseDependency] = Seq(
-    classOf[freechips.rocketchip.stage.phases.Checks],
-    classOf[freechips.rocketchip.stage.phases.TransformAnnotations],
-    classOf[freechips.rocketchip.stage.phases.PreElaboration],
-    classOf[chisel3.stage.phases.Checks],
-    classOf[chisel3.stage.phases.Elaborate],
-    classOf[freechips.rocketchip.stage.phases.GenerateROMs],
-    classOf[chisel3.stage.phases.AddImplicitOutputFile],
-    classOf[chisel3.stage.phases.AddImplicitOutputAnnotationFile],
-    classOf[chisel3.stage.phases.MaybeAspectPhase],
-    classOf[chisel3.stage.phases.Emitter],
-    classOf[chisel3.stage.phases.Convert],
-    classOf[freechips.rocketchip.stage.phases.GenerateFirrtlAnnos],
-    classOf[freechips.rocketchip.stage.phases.AddDefaultTests],
-    classOf[chipyard.stage.phases.AddDefaultTests],
-    classOf[chipyard.stage.phases.GenerateTestSuiteMakefrags],
-    classOf[freechips.rocketchip.stage.phases.GenerateArtefacts],
+    Dependency[freechips.rocketchip.stage.phases.Checks],
+    Dependency[freechips.rocketchip.stage.phases.TransformAnnotations],
+    Dependency[freechips.rocketchip.stage.phases.PreElaboration],
+    Dependency[chisel3.stage.phases.Checks],
+    Dependency[chisel3.stage.phases.Elaborate],
+    Dependency[freechips.rocketchip.stage.phases.GenerateROMs],
+    Dependency[chisel3.stage.phases.AddImplicitOutputFile],
+    Dependency[chisel3.stage.phases.AddImplicitOutputAnnotationFile],
+    Dependency[chisel3.stage.phases.MaybeAspectPhase],
+    Dependency[chisel3.stage.phases.Emitter],
+    Dependency[chisel3.stage.phases.Convert],
+    Dependency[freechips.rocketchip.stage.phases.GenerateFirrtlAnnos],
+    Dependency[freechips.rocketchip.stage.phases.AddDefaultTests],
+    Dependency[chipyard.stage.phases.AddDefaultTests],
+    Dependency[chipyard.stage.phases.GenerateTestSuiteMakefrags],
+    Dependency[freechips.rocketchip.stage.phases.GenerateArtefacts],
   )
 }
