422e7fd4e6aadba94016be3a0b8f3fa0156ac2cd
Colin Schmidt <colins@eecs.berkeley.edu>
2020-05-26 13:36:13 -0700

    Bump hammer for last pre-merge fixes, update make target names

--- vlsi/power.mk
+++ vlsi/power.mk
@@ -1,6 +1,6 @@
 .PHONY: $(POWER_CONF)
-power: $(POWER_CONF) sim-par
-power: override HAMMER_POWER_EXTRA_ARGS += -p $(POWER_CONF)
-redo-power: $(POWER_CONF)
-redo-power: override HAMMER_EXTRA_ARGS += -p $(POWER_CONF)
+power-par: $(POWER_CONF) sim-par
+power-par: override HAMMER_POWER_EXTRA_ARGS += -p $(POWER_CONF)
+redo-power-par: $(POWER_CONF)
+redo-power-par: override HAMMER_EXTRA_ARGS += -p $(POWER_CONF)
 $(OBJ_DIR)/power-rundir/power-output-full.json: private override HAMMER_EXTRA_ARGS += $(HAMMER_POWER_EXTRA_ARGS)
--- vlsi/sim.mk
+++ vlsi/sim.mk
@@ -1,9 +1,9 @@
 .PHONY: $(SIM_CONF) $(SIM_DEBUG_CONF) $(SIM_TIMING_CONF)
 # Update hammer top-level sim targets to include our generated sim configs
-redo-sim: $(SIM_CONF)
-redo-sim: override HAMMER_EXTRA_ARGS += -p $(SIM_CONF)
-redo-sim-debug: $(SIM_DEBUG_CONF) redo-sim
-redo-sim-debug: override HAMMER_EXTRA_ARGS += -p $(SIM_DEBUG_CONF)
+redo-sim-rtl: $(SIM_CONF)
+redo-sim-rtl: override HAMMER_EXTRA_ARGS += -p $(SIM_CONF)
+redo-sim-rtl-debug: $(SIM_DEBUG_CONF) redo-sim-rtl
+redo-sim-rtl-debug: override HAMMER_EXTRA_ARGS += -p $(SIM_DEBUG_CONF)
 
 redo-sim-syn: $(SIM_CONF)
 redo-sim-syn: override HAMMER_EXTRA_ARGS += -p $(SIM_CONF)
@@ -17,10 +17,10 @@ redo-sim-par-debug: override HAMMER_EXTRA_ARGS += -p $(SIM_DEBUG_CONF)
 redo-sim-par-timing-debug: $(SIM_TIMING_CONF) redo-sim-par-debug
 redo-sim-par-timing-debug: override HAMMER_EXTRA_ARGS += -p $(SIM_TIMING_CONF)
 
-sim: $(SIM_CONF)
-sim: override HAMMER_SIM_EXTRA_ARGS += -p $(SIM_CONF)
-sim-debug: $(SIM_DEBUG_CONF) sim
-sim-debug: override HAMMER_SIM_EXTRA_ARGS += -p $(SIM_DEBUG_CONF)
+sim-rtl: $(SIM_CONF)
+sim-rtl: override HAMMER_SIM_EXTRA_ARGS += -p $(SIM_CONF)
+sim-rtl-debug: $(SIM_DEBUG_CONF) sim-rtl
+sim-rtl-debug: override HAMMER_SIM_EXTRA_ARGS += -p $(SIM_DEBUG_CONF)
 $(OBJ_DIR)/sim-rundir/sim-output-full.json: private override HAMMER_EXTRA_ARGS += $(HAMMER_SIM_EXTRA_ARGS)
 
 sim-syn: $(SIM_CONF)
