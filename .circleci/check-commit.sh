93a06cc5e7ceeb888cb32f6e3efd0418083f6d38
Zitao Fang <zitaofang@berkeley.edu>
2020-10-01 10:11:04 -0700

    Fix CI master check

--- .circleci/check-commit.sh
+++ .circleci/check-commit.sh
@@ -48,7 +48,7 @@ search () {
     done
 }
 
-submodules=("ariane" "boom" "gemmini" "hwacha" "icenet" "nvdla" "rocket-chip" "sha3" "sifive-blocks" "sifive-cache" "testchipip" "sodor")
+submodules=("ariane" "boom" "gemmini" "hwacha" "icenet" "nvdla" "rocket-chip" "sha3" "sifive-blocks" "sifive-cache" "testchipip" "riscv-sodor")
 dir="generators"
 if [ "$CIRCLE_BRANCH" == "master" ] || [ "$CIRCLE_BRANCH" == "dev" ]
 then
