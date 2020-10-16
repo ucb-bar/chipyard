6eaac63e1be4035a395f3bf8e6c490794c58fad8
Alon Amid <alonamid@eecs.berkeley.edu>
2020-10-16 06:34:26 +0000

    address PR comments

--- generators/chipyard/src/main/scala/stage/ChipyardAnnotations.scala
+++ generators/chipyard/src/main/scala/stage/ChipyardAnnotations.scala
@@ -13,6 +13,7 @@ private[stage] object UnderscoreDelimitedConfigsAnnotation extends HasShellOptio
       longOption = "legacy-configs",
       toAnnotationSeq = a => {
         val split = a.split(':')
+        assert(split.length == 2)
         val packageName = split.head
         val configs     = split.last.split("_")
         Seq(new ConfigsAnnotation(configs map { config => if (config contains ".") s"${config}" else s"${packageName}.${config}" } ))
