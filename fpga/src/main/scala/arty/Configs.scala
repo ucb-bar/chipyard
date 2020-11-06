b0fc0457aa63073b10b86d4cccb21e45d428fc8c
abejgonzalez <abe.j.gonza@gmail.com>
2020-11-05 20:44:48 -0800

    Use Chipyard configs as base (Arty)

--- fpga/src/main/scala/arty/Configs.scala
+++ fpga/src/main/scala/arty/Configs.scala
@@ -11,6 +11,8 @@ import freechips.rocketchip.tile._
 
 import sifive.blocks.devices.uart._
 
+import testchipip.{SerialTLKey}
+
 import chipyard.{BuildSystem}
 
 class WithDefaultPeripherals extends Config((site, here, up) => {
@@ -22,29 +24,20 @@ class WithDefaultPeripherals extends Config((site, here, up) => {
     idcodePartNum = 0x000,
     idcodeManufId = 0x489,
     debugIdleCycles = 5)
+  case SerialTLKey => None // remove serialized tl port
 })
 
-class TinyRocketArtyConfig extends Config(
+class WithArtyTweaks extends Config(
   new WithArtyJTAGHarnessBinder ++
   new WithArtyUARTHarnessBinder ++
   new WithArtyResetHarnessBinder ++
-  new chipyard.iobinders.WithDebugIOCells ++
-  new chipyard.iobinders.WithUARTIOCells ++
   new WithResetPassthrough ++
   new WithDefaultPeripherals ++
-  new chipyard.config.WithNoSubsystemDrivenClocks ++
-  new chipyard.config.WithPeripheryBusFrequencyAsDefault ++
-  new chipyard.config.WithBootROM ++
-  new chipyard.config.WithL2TLBs(1024) ++
-  new freechips.rocketchip.subsystem.With1TinyCore ++
-  new freechips.rocketchip.subsystem.WithNBanks(0) ++
-  new freechips.rocketchip.subsystem.WithNoMemPort ++
-  new freechips.rocketchip.subsystem.WithNMemoryChannels(0) ++
+  new freechips.rocketchip.subsystem.WithNBanks(0) ++ // remove L2$
+  new freechips.rocketchip.subsystem.WithNoMemPort ++ // remove backing memory
   new freechips.rocketchip.subsystem.WithNBreakpoints(2) ++
-  new freechips.rocketchip.subsystem.WithJtagDTM ++
-  new freechips.rocketchip.subsystem.WithNoMMIOPort ++
-  new freechips.rocketchip.subsystem.WithNoSlavePort ++
-  new freechips.rocketchip.subsystem.WithInclusiveCache ++
-  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
-  new freechips.rocketchip.subsystem.WithIncoherentBusTopology ++
-  new freechips.rocketchip.system.BaseConfig)
+  new freechips.rocketchip.subsystem.WithIncoherentBusTopology)
+
+class TinyRocketArtyConfig extends Config(
+  new WithArtyTweaks ++
+  new chipyard.TinyRocketConfig)
