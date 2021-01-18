2272a34510f754197812652753286f46f7c570e0
abejgonzalez <abe.j.gonza@gmail.com>
2021-01-18 12:18:24 -0800

    Bump Gemmini | Add BOOM Lab 2 config

--- generators/chipyard/src/main/scala/config/EE290Configs.scala
+++ generators/chipyard/src/main/scala/config/EE290Configs.scala
@@ -7,9 +7,16 @@ import freechips.rocketchip.config.{Config}
 // --------------------
 // EE290 Rocket Configs
 // --------------------
+// default lab configuration
 class GemminiEE290Lab2RocketConfig extends Config(
   new gemmini.GemminiEE290Lab2Config ++
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+// much slower to elaborate but faster to run
+class GemminiEE290Lab2BoomConfig extends Config(
+  new gemmini.GemminiEE290Lab2Config ++
+  new boom.common.WithNLargeBooms(1) ++
   new chipyard.config.AbstractConfig)
 
 class GemminiEE290Lab2BigSPRocketConfig extends Config(
