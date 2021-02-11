eb125a65d63a1dcf5a8ca688b2a23e1c0a6944b3
abejgonzalez <abe.j.gonza@gmail.com>
2021-02-11 15:09:57 -0800

    Add BOOM configs for Lab3 | Misc. cleanup

--- generators/chipyard/src/main/scala/config/EE290Configs.scala
+++ generators/chipyard/src/main/scala/config/EE290Configs.scala
@@ -7,22 +7,40 @@ import freechips.rocketchip.config.{Config}
 // --------------------
 // EE290 Rocket Configs
 // --------------------
-// default lab configuration
+
+// -----
+// Lab 2
+// -----
+
+// Rocket-based
+
 class GemminiEE290Lab2RocketConfig extends Config(
   new gemmini.GemminiEE290Lab2Config ++
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
-  new chipyard.config.AbstractConfig)
-
-// much slower to elaborate but faster to run
-class GemminiEE290Lab2BoomConfig extends Config(
-  new gemmini.GemminiEE290Lab2Config ++
-  new boom.common.WithNLargeBooms(1) ++
   new chipyard.config.AbstractConfig)
 
 class GemminiEE290Lab2BigSPRocketConfig extends Config(
   new gemmini.GemminiEE290Lab2LargeSPConfig ++
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
   new chipyard.config.AbstractConfig)
+
+// BOOM-based
+
+class GemminiEE290Lab2BoomConfig extends Config(
+  new gemmini.GemminiEE290Lab2Config ++
+  new boom.common.WithNLargeBooms(1) ++
+  new chipyard.config.AbstractConfig)
+
+class GemminiEE290Lab2BigSPBoomConfig extends Config(
+  new gemmini.GemminiEE290Lab2LargeSPConfig ++
+  new boom.common.WithNLargeBooms(1) ++
+  new chipyard.config.AbstractConfig)
+
+// -----
+// Lab 3
+// -----
+
+// Rocket-based
 
 class GemminiEE290Lab3RocketConfig extends Config(
   new gemmini.GemminiEE290Lab3Config ++
@@ -32,4 +50,16 @@ class GemminiEE290Lab3RocketConfig extends Config(
 class GemminiEE290Lab3SmallSPRocketConfig extends Config(
   new gemmini.GemminiEE290Lab3SmallSPConfig ++
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+// BOOM-based
+
+class GemminiEE290Lab3BoomConfig extends Config(
+  new gemmini.GemminiEE290Lab3Config ++
+  new boom.common.WithNLargeBooms(1) ++
+  new chipyard.config.AbstractConfig)
+
+class GemminiEE290Lab3SmallSPBoomConfig extends Config(
+  new gemmini.GemminiEE290Lab3SmallSPConfig ++
+  new boom.common.WithNLargeBooms(1) ++
   new chipyard.config.AbstractConfig)
