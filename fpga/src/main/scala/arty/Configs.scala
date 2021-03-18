f8660d13582a0c1cf1a2561056edeb33d3cfd695
James Dunn <jhdunn@berkeley.edu>
2021-03-18 13:17:19 -0700

    First pass at XIP bootrom for arty. Mostly cribbed from a combination of freedom and Abe's VCU118 sdboot. Builds but need to test.

--- fpga/src/main/scala/arty/Configs.scala
+++ fpga/src/main/scala/arty/Configs.scala
@@ -1,10 +1,12 @@
 // See LICENSE for license details.
 package chipyard.fpga.arty
 
+import sys.process._
+
 import freechips.rocketchip.config._
 import freechips.rocketchip.subsystem._
 import freechips.rocketchip.devices.debug._
-import freechips.rocketchip.devices.tilelink._
+import freechips.rocketchip.devices.tilelink.{BootROMLocated}
 import freechips.rocketchip.diplomacy.{DTSModel, DTSTimebase}
 import freechips.rocketchip.system._
 import freechips.rocketchip.tile._
@@ -31,6 +33,12 @@ class WithDefaultPeripherals extends Config((site, here, up) => {
       fAddress = 0x20000000,
       rAddress = 0x10014000,
       defaultSampleDel = 3))
+  case BootROMLocated(x) => up(BootROMLocated(x), site).map { p =>
+    // invoke makefile for xip
+    val make = s"make -C fpga/src/main/resources/arty/xip bin"
+    require (make.! == 0, "Failed to build bootrom")
+    p.copy(hang = 0x10000, contentFileName = s"./fpga/src/main/resources/arty/xip/build/xip.bin")
+  }
 })
 // DOC include start: AbstractArty and Rocket
 class WithArtyTweaks extends Config(
