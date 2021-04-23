975d6c09ac5927c3e3bc9e41e440da05850e5b47
James Dunn <dunn@eecs.berkeley.edu>
2021-04-23 13:43:47 -0500

    More collateral generation.

--- fpga/scripts/generate_vcs_collateral.tcl
+++ fpga/scripts/generate_vcs_collateral.tcl
@@ -1,5 +1,5 @@
 #### Command line arguments to this script
-# argv[0] = fpga family 
+# argv[0] = fpga family
 # argv[1] = build directory
 # argv[2] = chipyard model definition
 
@@ -13,3 +13,14 @@ compile_simlib -directory $build_dir/$model.cache/compile_simlib -family $family
 set_property top ArtyTestDriver [current_fileset -simset]
 # generate other vcs simulation collateral
 export_simulation -force -simulator vcs -ip_user_files_dir $build_dir/$model.ip_user_files -lib_map_path $build_dir/$model.cache/compile_simlib -use_ip_compiled_libs -directory $build_dir/export_sim
+# Add vivado library mapping to synopsys_sim.setup file
+set synopsys_libraries [open $build_dir/synopsys_sim.setup a]
+puts $synopsys_libraries "xil_defaultlib : vcs_lib/xil_defaultlib"
+close $synopsys_libraries
+#generate separate lists of verilog and vhdl sim sources
+set fpga_sim_verilog_sources [open $build_dir/fpga_sim_verilog_sources.f w]
+foreach source [get_files -compile_order sources -used_in simulation -filter {FILE_TYPE == Verilog}] {puts $fpga_sim_verilog_sources $source}
+close $fpga_sim_verilog_sources
+set fpga_sim_vhdl_sources [open $build_dir/fpga_sim_vhdl_sources.f w]
+foreach source [get_files -compile_order sources -used_in simulation -filter {FILE_TYPE == VHDL}] {puts $fpga_sim_vhdl_sources $source}
+close $fpga_sim_vhdl_sources
