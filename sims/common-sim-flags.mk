1d52899736a01ed27b4415d5e33c9ec73a4e0568
abejgonzalez <abe.j.gonza@gmail.com>
2021-05-06 00:27:11 -0700

    Remove GenerateSimFiles and use make instead

--- sims/common-sim-flags.mk
+++ sims/common-sim-flags.mk
@@ -21,3 +21,19 @@ SIM_LDFLAGS = \
 	-lfesvr \
 	-ldramsim \
 	$(EXTRA_SIM_LDFLAGS)
+
+SIM_FILE_REQS += \
+	$(TESTCHIP_RSRCS_DIR)/testchipip/csrc/SimSerial.cc \
+	$(TESTCHIP_RSRCS_DIR)/testchipip/csrc/SimDRAM.cc \
+	$(TESTCHIP_RSRCS_DIR)/testchipip/csrc/testchip_tsi.cc \
+	$(TESTCHIP_RSRCS_DIR)/testchipip/csrc/testchip_tsi.h \
+	$(TESTCHIP_RSRCS_DIR)/testchipip/csrc/mm.h \
+	$(TESTCHIP_RSRCS_DIR)/testchipip/csrc/mm.cc \
+	$(TESTCHIP_RSRCS_DIR)/testchipip/csrc/mm_dramsim2.h \
+	$(TESTCHIP_RSRCS_DIR)/testchipip/csrc/mm_dramsim2.cc \
+	$(ROCKETCHIP_RSRCS_DIR)/vsrc/EICG_wrapper.v \
+    $(ROCKETCHIP_RSRCS_DIR)/csrc/SimDTM.cc \
+    $(ROCKETCHIP_RSRCS_DIR)/csrc/SimJTAG.cc \
+    $(ROCKETCHIP_RSRCS_DIR)/csrc/remote_bitbang.h \
+    $(ROCKETCHIP_RSRCS_DIR)/csrc/remote_bitbang.cc
+
