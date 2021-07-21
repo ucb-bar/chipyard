9473a0cc1768c6b41f84ec0f51f7348c61c44a0a
John Wright <johnwright@eecs.berkeley.edu>
2021-07-21 11:33:25 -0700

    API tweaks, add Hwacha shell for example

--- generators/chipyard/src/main/scala/floorplan/ChipTopFloorplans.scala
+++ generators/chipyard/src/main/scala/floorplan/ChipTopFloorplans.scala
@@ -10,14 +10,14 @@ object ChipTopFloorplans {
 
   def default: FloorplanFunction = {
     case top: ChipTopLazyRawModuleImp =>
-      val context = Floorplan(top)
+      val context = Floorplan(top, 500.0, 500.0)
       val topGrid = context.setTopGroup(context.createElasticArray(2))
       val tiles = top.outer.lazySystem match {
         case t: DigitalTop => t.tiles.map(x => context.addHier(x.module))
         case _ => throw new Exception("Unsupported BuildSystem type")
       }
-      topGrid.placeAt(context.createElasticArray(tiles), 1)
-      topGrid.placeAt(context.createSpacer(Some("spacer")), 0)
+      topGrid.placeAt(1, context.createElasticArray(tiles))
+      topGrid.placeAt(0, context.createSpacer(Some("spacer")))
       context.elements
   }
 
--- generators/chipyard/src/main/scala/floorplan/RocketFloorplans.scala
+++ generators/chipyard/src/main/scala/floorplan/RocketFloorplans.scala
@@ -2,6 +2,7 @@
 package chipyard.floorplan
 
 import freechips.rocketchip.tile.{RocketTileModuleImp}
+import hwacha.{Hwacha, HwachaImp}
 import barstools.floorplan.chisel.{FloorplanAspect, Floorplan, FloorplanFunction}
 
 object RocketFloorplans {
@@ -10,8 +11,21 @@ object RocketFloorplans {
     case tile: RocketTileModuleImp =>
       val context = Floorplan(tile)
       val topGroup = context.setTopGroup(context.createElasticArray(3))
-      val memArray = topGroup.placeAt(context.createMemArray(Some("l1_icache_data")), 0)
+      val memArray = topGroup.placeAt(0, context.createMemArray(Some("l1_icache_data")))
+      topGroup.placeAt(1, context.createSpacer(Some("spacer")))
       tile.outer.frontend.icache.module.data_arrays.map(x => memArray.addMem(x._1))
+      // Add more SRAM arrays here
+
+      // Add optional accelerator placements
+      val hwacha = tile.outer.roccs.collectFirst { case h: Hwacha =>
+        topGroup.placeAt(2, context.addHier(h.module))
+      }
+
+      context.elements
+    case hwacha: HwachaImp =>
+      val context = Floorplan(hwacha)
+      val topGroup = context.setTopGroup(context.createElasticArray(2))
+      // Hwacha floorplan goes here
       context.elements
   }
 }
