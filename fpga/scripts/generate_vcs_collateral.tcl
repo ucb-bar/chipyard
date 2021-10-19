c2d00ae2efd2d30773d7bb2f8e5f37c73bb65e3d
dunn <dunn@eecs.berkeley.edu>
2021-10-18 17:45:36 -0700

    Parameterizing testbench in sim flow

--- fpga/scripts/generate_vcs_collateral.tcl
+++ fpga/scripts/generate_vcs_collateral.tcl
@@ -2,22 +2,24 @@
 # argv[0] = fpga family
 # argv[1] = build directory
 # argv[2] = chipyard model definition
+# argv[3] = test driver
 
 set family [lindex $argv 0]
 set build_dir [lindex $argv 1]
 set model [lindex $argv 2]
+set tb [lindex $argv 3]
 
 # compile the simulation libraries for vcs
 compile_simlib -directory $build_dir/$model.cache/compile_simlib -family $family -simulator vcs_mx -library all
 # set the top level of the design
-set_property top ArtyTestDriver [current_fileset -simset]
+set_property top $tb [current_fileset -simset]
 # generate other vcs simulation collateral
 export_simulation -force -simulator vcs -ip_user_files_dir $build_dir/$model.ip_user_files -lib_map_path $build_dir/$model.cache/compile_simlib -use_ip_compiled_libs -directory $build_dir/export_sim
 # Add vivado library mapping to synopsys_sim.setup file
 set synopsys_libraries [open $build_dir/synopsys_sim.setup a]
 puts $synopsys_libraries "xil_defaultlib : vcs_lib/xil_defaultlib"
 close $synopsys_libraries
-#generate separate lists of verilog, vhdl, and cc sim sources
+# generate separate lists of verilog, vhdl, and cc sim sources
 set fpga_sim_verilog_sources [open $build_dir/fpga_sim_verilog_sources.f w]
 foreach source [get_files -compile_order sources -used_in simulation -filter {FILE_TYPE == Verilog}] {puts $fpga_sim_verilog_sources $source}
 foreach source [get_files -compile_order sources -used_in simulation -filter {FILE_TYPE == SystemVerilog}] {puts $fpga_sim_verilog_sources $source}
