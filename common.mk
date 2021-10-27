23bb89e21ea3936805b5cfc7fa77c494bc2a3574
Abraham Gonzalez <abe.j.gonza@gmail.com>
2021-10-26 23:15:18 -0700

    Forgot a couple of sim prereqs

--- common.mk
+++ common.mk
@@ -219,19 +219,19 @@ $(binary_hex): $(output_dir) $(BINARY)
 	$(base_dir)/scripts/smartelf2hex.sh $(BINARY) > $(binary_hex)
 
 run-binary-hex: check-binary
-run-binary-hex: $(output_dir) $(sim) $(binary_hex)
+run-binary-hex: $(output_dir) $(SIM_PREREQ) $(binary_hex)
 run-binary-hex: run-binary
 run-binary-hex: override LOADMEM_ADDR = 80000000
 run-binary-hex: override LOADMEM = $(binary_hex)
 run-binary-hex: override SIM_FLAGS += +loadmem=$(LOADMEM) +loadmem_addr=$(LOADMEM_ADDR)
 run-binary-debug-hex: check-binary
-run-binary-debug-hex: $(output_dir) $(sim) $(binary_hex)
+run-binary-debug-hex: $(output_dir) $(SIM_DEBUG_REREQ) $(binary_hex)
 run-binary-debug-hex: run-binary-debug
 run-binary-debug-hex: override LOADMEM_ADDR = 80000000
 run-binary-debug-hex: override LOADMEM = $(binary_hex)
 run-binary-debug-hex: override SIM_FLAGS += +loadmem=$(LOADMEM) +loadmem_addr=$(LOADMEM_ADDR)
 run-binary-fast-hex: check-binary
-run-binary-fast-hex: $(output_dir) $(binary_hex)
+run-binary-fast-hex: $(output_dir) $(SIM_PREREQ) $(binary_hex)
 run-binary-fast-hex: run-binary-fast
 run-binary-fast-hex: override LOADMEM_ADDR = 80000000
 run-binary-fast-hex: override LOADMEM = $(binary_hex)
