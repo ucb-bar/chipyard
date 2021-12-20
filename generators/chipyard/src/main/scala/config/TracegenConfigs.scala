42e7c1f30b73f4c9c86ae9b67f34ad37c07f89dc
Jerry Zhao <jerryz123@berkeley.edu>
2021-12-20 11:19:15 -0800

    Add tracegen for bar-cache

--- generators/chipyard/src/main/scala/config/TracegenConfigs.scala
+++ generators/chipyard/src/main/scala/config/TracegenConfigs.scala
@@ -29,7 +29,11 @@ class NonBlockingTraceGenConfig extends Config(
 
 class BoomTraceGenConfig extends Config(
   new tracegen.WithBoomTraceGen()(List.fill(2) { DCacheParams(nMSHRs = 8, nSets = 16, nWays = 2) }) ++
-  new freechips.rocketchip.subsystem.WithInclusiveCache ++
+  new AbstractTraceGenConfig)
+
+class BarCacheTraceGenConfig extends Config(
+  new tracegen.WithTraceGen()(List.fill(2) { DCacheParams(nMSHRs = 8, nSets = 16, nWays = 2) }) ++
+  new barcache.WithBarCache((0 until 2)) ++
   new AbstractTraceGenConfig)
 
 class NonBlockingTraceGenL2Config extends Config(
@@ -37,8 +41,13 @@ class NonBlockingTraceGenL2Config extends Config(
   new freechips.rocketchip.subsystem.WithInclusiveCache ++
   new AbstractTraceGenConfig)
 
-class NonBlockingTraceGenL2RingConfig extends Config(
-  new tracegen.WithL2TraceGen()(List.fill(2)(DCacheParams(nMSHRs = 2, nSets = 16, nWays = 4))) ++
-  new testchipip.WithRingSystemBus ++
+class BoomTraceGenL2Config extends Config(
+  new tracegen.WithBoomTraceGen()(List.fill(2) { DCacheParams(nMSHRs = 8, nSets = 16, nWays = 2) }) ++
+  new freechips.rocketchip.subsystem.WithInclusiveCache ++
+  new AbstractTraceGenConfig)
+
+class BarCacheTraceGenL2Config extends Config(
+  new tracegen.WithTraceGen()(List.fill(2) { DCacheParams(nMSHRs = 8, nSets = 16, nWays = 2) }) ++
+  new barcache.WithBarCache((0 until 2)) ++
   new freechips.rocketchip.subsystem.WithInclusiveCache ++
   new AbstractTraceGenConfig)
