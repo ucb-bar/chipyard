c1262a279ee770e6ae6c4d3f4d3a19160fc22bca
Jerry Zhao <jerryz123@berkeley.edu>
2022-02-11 15:04:10 -0800

    Update constellation

--- generators/chipyard/src/main/scala/config/NoCConfigs.scala
+++ generators/chipyard/src/main/scala/config/NoCConfigs.scala
@@ -3,15 +3,15 @@ package chipyard
 import freechips.rocketchip.config.{Config}
 import freechips.rocketchip.diplomacy.{AsynchronousCrossing}
 
-import constellation.{UserVirtualChannelParams}
-import constellation.routing.{RoutingRelations}
+import constellation.channel.{UserVirtualChannelParams}
+import constellation.routing.{RoutingRelation}
 
 
 class DualRocketNoCConfig extends Config(
   new constellation.WithConstellationNoCSystemBus(Seq(0, 0, 1), Seq(0, 2, 3, 4, 5)) ++
-  new constellation.WithNBlockingVirtualNetworks(5) ++
-  new constellation.WithUniformVirtualChannels(6, UserVirtualChannelParams(3)) ++
-  new constellation.Mesh2DConfig(2, 3, RoutingRelations.mesh2DEscapeRouter) ++
+  new constellation.routing.WithNBlockingVirtualNetworks(5) ++
+  new constellation.channel.WithUniformVirtualChannels(6, UserVirtualChannelParams(3)) ++
+  new constellation.topology.Mesh2DConfig(2, 3, RoutingRelation.mesh2DEscapeRouter) ++
   new freechips.rocketchip.subsystem.WithNonblockingL1(4) ++
   new freechips.rocketchip.subsystem.WithNBigCores(2) ++
   new freechips.rocketchip.subsystem.WithNBanks(4) ++
@@ -19,9 +19,9 @@ class DualRocketNoCConfig extends Config(
 
 class QuadRocketNoCConfig extends Config(
   new constellation.WithConstellationNoCSystemBus(Seq(0, 0, 1, 2, 3), Seq(0) ++ Seq.tabulate(16) { _ + 4 }) ++
-  new constellation.WithNBlockingVirtualNetworks(5) ++
-  new constellation.WithUniformVirtualChannels(6, UserVirtualChannelParams(3)) ++
-  new constellation.Mesh2DConfig(4, 5, RoutingRelations.mesh2DEscapeRouter) ++
+  new constellation.routing.WithNBlockingVirtualNetworks(5) ++
+  new constellation.channel.WithUniformVirtualChannels(6, UserVirtualChannelParams(3)) ++
+  new constellation.topology.Mesh2DConfig(4, 5, RoutingRelation.mesh2DEscapeRouter) ++
   new freechips.rocketchip.subsystem.WithNonblockingL1(4) ++
   new freechips.rocketchip.subsystem.WithNBigCores(4) ++
   new freechips.rocketchip.subsystem.WithInclusiveCache(nBanks=16, capacityKB=1024) ++
@@ -30,11 +30,11 @@ class QuadRocketNoCConfig extends Config(
 
 class HexaRocketNoCConfig extends Config(
   new constellation.WithConstellationNoCSystemBus(Seq(4, 0, 2, 5, 6, 9, 11), Seq(7, 1, 3, 8, 10)) ++
-  new constellation.WithNBlockingVirtualNetworks(5) ++
-  new constellation.WithCombineRCVA ++
-  new constellation.WithCombineSAST ++
-  new constellation.WithUniformVirtualChannels(6, UserVirtualChannelParams(3)) ++
-  new constellation.Mesh2DConfig(4, 3, RoutingRelations.mesh2DEscapeRouter) ++
+  new constellation.routing.WithNBlockingVirtualNetworks(5) ++
+  new constellation.router.WithCombineRCVA ++
+  new constellation.router.WithCombineSAST ++
+  new constellation.channel.WithUniformVirtualChannels(6, UserVirtualChannelParams(3)) ++
+  new constellation.topology.Mesh2DConfig(4, 3, RoutingRelation.mesh2DEscapeRouter) ++
   new freechips.rocketchip.subsystem.WithNonblockingL1(4) ++
   new freechips.rocketchip.subsystem.WithNBigCores(6) ++
   new freechips.rocketchip.subsystem.WithNBanks(4) ++
