5d7b43f6db1d553cbacb417edd18f401e96c8f32
Jerry Zhao <jerryz123@berkeley.edu>
2022-03-14 16:59:08 -0700

    Bump NoCConfigs

--- generators/chipyard/src/main/scala/config/NoCConfigs.scala
+++ generators/chipyard/src/main/scala/config/NoCConfigs.scala
@@ -8,34 +8,31 @@ import constellation.routing.{RoutingRelation}
 
 
 class DualRocketNoCConfig extends Config(
-  new constellation.WithConstellationNoCSystemBus(Seq(0, 0, 1), Seq(0, 2, 3, 4, 5)) ++
+  new constellation.rc.WithConstellationNoCSystemBus(Seq(0, 0, 1), Seq(0, 2, 3, 4, 5)) ++
   new constellation.routing.WithNBlockingVirtualNetworks(5) ++
   new constellation.channel.WithUniformNVirtualChannels(6, UserVirtualChannelParams(3)) ++
-  new constellation.topology.Mesh2DConfig(2, 3, RoutingRelation.mesh2DEscapeRouter) ++
-  new freechips.rocketchip.subsystem.WithNonblockingL1(4) ++
+  new constellation.topology.WithMesh2DTopology(2, 3, RoutingRelation.mesh2DEscapeRouter) ++
   new freechips.rocketchip.subsystem.WithNBigCores(2) ++
   new freechips.rocketchip.subsystem.WithNBanks(4) ++
   new chipyard.config.AbstractConfig)
 
 class QuadRocketNoCConfig extends Config(
-  new constellation.WithConstellationNoCSystemBus(Seq(0, 0, 1, 2, 3), Seq(0) ++ Seq.tabulate(16) { _ + 4 }) ++
+  new constellation.rc.WithConstellationNoCSystemBus(Seq(0, 0, 1, 2, 3), Seq(0) ++ Seq.tabulate(16) { _ + 4 }) ++
   new constellation.routing.WithNBlockingVirtualNetworks(5) ++
   new constellation.channel.WithUniformNVirtualChannels(6, UserVirtualChannelParams(3)) ++
-  new constellation.topology.Mesh2DConfig(4, 5, RoutingRelation.mesh2DEscapeRouter) ++
-  new freechips.rocketchip.subsystem.WithNonblockingL1(4) ++
+  new constellation.topology.WithMesh2DTopology(4, 5, RoutingRelation.mesh2DEscapeRouter) ++
   new freechips.rocketchip.subsystem.WithNBigCores(4) ++
   new freechips.rocketchip.subsystem.WithInclusiveCache(nBanks=16, capacityKB=1024) ++
   new chipyard.config.AbstractConfig)
 
 
 class HexaRocketNoCConfig extends Config(
-  new constellation.WithConstellationNoCSystemBus(Seq(4, 0, 2, 5, 6, 9, 11), Seq(7, 1, 3, 8, 10)) ++
+  new constellation.rc.WithConstellationNoCSystemBus(Seq(4, 0, 2, 5, 6, 9, 11), Seq(7, 1, 3, 8, 10)) ++
   new constellation.routing.WithNBlockingVirtualNetworks(5) ++
   new constellation.router.WithCombineRCVA ++
   new constellation.router.WithCombineSAST ++
   new constellation.channel.WithUniformNVirtualChannels(6, UserVirtualChannelParams(3)) ++
-  new constellation.topology.Mesh2DConfig(4, 3, RoutingRelation.mesh2DEscapeRouter) ++
-  new freechips.rocketchip.subsystem.WithNonblockingL1(4) ++
+  new constellation.topology.WithMesh2DTopology(4, 3, RoutingRelation.mesh2DEscapeRouter) ++
   new freechips.rocketchip.subsystem.WithNBigCores(6) ++
   new freechips.rocketchip.subsystem.WithNBanks(4) ++
   new chipyard.config.AbstractConfig)
