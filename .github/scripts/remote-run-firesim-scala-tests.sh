54423cca013047b0cae9264b105fb433ad657a28
Abraham Gonzalez <abe.gonzalez@berkeley.edu>
2022-09-01 18:49:04 +0000

    Use libelf/dwarf from conda

--- .github/scripts/remote-run-firesim-scala-tests.sh
+++ .github/scripts/remote-run-firesim-scala-tests.sh
@@ -10,24 +10,10 @@ set -ex
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 source $SCRIPT_DIR/defaults.sh
 
-# Directory locations for handling firesim-local installations of libelf/libdwarf
-# This would generally be handled by build-setup.sh/firesim-setup.sh
-REMOTE_FIRESIM_SYSROOT=$REMOTE_FIRESIM_DIR/lib-install
-
 cd $REMOTE_CHIPYARD_DIR
 ./scripts/init-submodules-no-riscv-tools.sh --skip-validate
-cd $REMOTE_CHIPYARD_DIR/sims/firesim/sim/firesim-lib/src/main/cc/lib
-git submodule update --init elfutils libdwarf
-cd $REMOTE_CHIPYARD_DIR/sims/firesim
-mkdir -p $REMOTE_FIRESIM_SYSROOT
-./scripts/build-libelf.sh $REMOTE_FIRESIM_SYSROOT
-./scripts/build-libdwarf.sh $REMOTE_FIRESIM_SYSROOT
-cd $REMOTE_CHIPYARD_DIR
-
-make -C $REMOTE_CHIPYARD_DIR/tools/dromajo/dromajo-src/src
 
 # Run Firesim Scala Tests
-export LD_LIBRARY_PATH=$REMOTE_FIRESIM_SYSROOT/lib:$LD_LIBRARY_PATH
 export FIRESIM_ENV_SOURCED=1;
 export COURSIER_CACHE=$REMOTE_WORK_DIR/.coursier-cache
 make -C $REMOTE_FIRESIM_DIR JAVA_OPTS="$REMOTE_JAVA_OPTS" SBT_OPTS="$REMOTE_SBT_OPTS" testOnly ${mapping[$1]}
