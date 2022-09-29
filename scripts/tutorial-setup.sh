7a14303aa977da0e0c241a32498349aa47e5daca
Nayiri K <nayiri@berkeley.edu>
2022-09-29 15:58:30 -0700

    another one

--- scripts/tutorial-setup.sh
+++ scripts/tutorial-setup.sh
@@ -9,27 +9,30 @@ export TUTORIAL_INSTALL_PATH=$(pwd)
 # # >>>>>>>>> THIS WORKS
 # # Sky130 PDK
 # echo "installing sky130A PDK"
-# wget https://github.com/nayiri-k/hammer-workspace/raw/main/tech/sky130A.tar.bz2
+# wget -q https://github.com/nayiri-k/hammer-workspace/raw/main/tech/sky130A.tar.bz2
 # tar -xf sky130A.tar.bz2
+
+# # Sky130 SRAMs
+# echo "installing sky130 sram macros"
+# git clone https://github.com/efabless/sky130_sram_macros.git
+# # Yosys
+# # conda create --name yosys --no-default-packages -y
+# echo "installing yosys"
+# # conda install -c timvideos -y yosys
+# wget -q https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2022-09-29/oss-cad-suite-linux-x64-20220929.tgz
+# tar zxf oss-cad-suite-linux-x64-20220929.tgz
+# export PATH=$TUTORIAL_INSTALL_PATH/oss-cad-suite/bin:$PATH
+# yosys -help
 # # <<<<<<<<< THIS WORKS
-# Sky130 SRAMs
-echo "installing sky130 sram macros"
-git clone https://github.com/efabless/sky130_sram_macros.git
-# Yosys
-# conda create --name yosys --no-default-packages -y
-echo "installing yosys"
-# conda install -c timvideos -y yosys
-wget https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2022-09-29/oss-cad-suite-linux-x64-20220929.tgz
-tar zxf oss-cad-suite-linux-x64-20220929.tgz
-export PATH=$TUTORIAL_INSTALL_PATH/oss-cad-suite/bin:$PATH
-yosys -help
+
 # OpenROAD
 # first install dependencies
 echo "installing openroad dependencies"
-conda install -y -c anaconda libffi
-conda install -y -c intel tcl
+which time
 conda install -y -c conda-forge time
 conda install -y -c anaconda pandas
+conda install -y -c intel tcl
+conda install -y -c anaconda libffi
 # build openroad
 echo "installing openroad"
 git clone --recursive https://github.com/The-OpenROAD-Project/OpenROAD.git
@@ -43,11 +46,17 @@ openroad -help
 # KLayout
 echo "installing klayout"
 cd $TUTORIAL_INSTALL_PATH
-wget https://www.klayout.org/downloads/source/klayout-0.27.1.tar.gz
+wget -q https://www.klayout.org/downloads/source/klayout-0.27.1.tar.gz
 tar zxvf klayout-0.27.1.tar.gz
 cd klayout-0.27.1
 ./build.sh -without-qtbinding
 ls
+
+echo "end of tutorial setup testing"
+
+
+
+
 
 set -ex
 
