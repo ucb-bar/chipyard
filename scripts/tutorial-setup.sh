60dab943b27984d078bb414c1af7475cb8ea2a59
Nayiri K <nayiri@berkeley.edu>
2022-09-29 18:06:10 -0700

    we are parallelizing this

--- scripts/tutorial-setup.sh
+++ scripts/tutorial-setup.sh
@@ -6,6 +6,15 @@ echo "making tutorial directory"
 mkdir -p ~/tutorial-installs
 cd ~/tutorial-installs
 export TUTORIAL_INSTALL_PATH=$(pwd)
+
+# KLayout
+echo "installing klayout"
+cd $TUTORIAL_INSTALL_PATH
+wget -q https://www.klayout.org/downloads/source/klayout-0.27.1.tar.gz
+tar zxvf klayout-0.27.1.tar.gz
+cd klayout-0.27.1
+./build.sh -without-qtbinding
+ls
 # # >>>>>>>>> THIS WORKS
 # # Sky130 PDK
 # echo "installing sky130A PDK"
@@ -29,15 +38,6 @@ export TUTORIAL_INSTALL_PATH=$(pwd)
 # OpenROAD
 # first install dependencies
 echo "installing openroad dependencies"
-conda install -y -c conda-forge time
-
-# wget -q https://prdownloads.sourceforge.net/tcl/tcl8.6.12-src.tar.gz
-# gunzip < tcl8.6.12-src.tar.gz | tar xf -
-# cd tcl8.6.12/unix
-# ./configure --prefix=$TUTORIAL_INSTALL_PATH
-# make
-# make test
-# make install
 
 git clone --recursive https://github.com/The-OpenROAD-Project/OpenROAD.git
 cd OpenROAD
@@ -46,10 +46,6 @@ mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$TUTORIAL_INSTALL_PATH 
 make
-# conda install -y -c conda-forge time
-# conda install -y -c intel tcl
-# conda install -y -c anaconda pandas
-# conda install -y -c anaconda libffi
 
 # build openroad
 # echo "installing openroad"
@@ -61,15 +57,6 @@ make
 # make install
 export PATH=$TUTORIAL_INSTALL_PATH/bin:$PATH
 openroad -help
-
-# KLayout
-echo "installing klayout"
-cd $TUTORIAL_INSTALL_PATH
-wget -q https://www.klayout.org/downloads/source/klayout-0.27.1.tar.gz
-tar zxvf klayout-0.27.1.tar.gz
-cd klayout-0.27.1
-./build.sh -without-qtbinding
-ls
 
 echo "end of tutorial setup testing"
 
