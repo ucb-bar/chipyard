576d4942794e6396f109eae1f9d423beef2ccc84
nayiri-k <nayiri@berkeley.edu>
2022-09-29 20:45:54 -0700

    pls work now

--- scripts/tutorial-setup.sh
+++ scripts/tutorial-setup.sh
@@ -13,54 +13,68 @@ export PATH=$TUTORIAL_INSTALL_PATH/bin:$PATH
 echo "installing klayout"
 cd $TUTORIAL_INSTALL_PATH
 sudo apt-get update
-sudo apt-get install -y klayout
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install klayout
+klayout -h
+klayout -v
+which klayout
 # wget -q https://www.klayout.org/downloads/source/klayout-0.27.1.tar.gz
 # tar zxf klayout-0.27.1.tar.gz
 # cd klayout-0.27.1
 # ./build.sh -without-qtbinding
 # ls
 
-# # >>>>>>>>> THIS WORKS
-# # Sky130 PDK
-# echo "installing sky130A PDK"
-# wget -q https://github.com/nayiri-k/hammer-workspace/raw/main/tech/sky130A.tar.bz2
-# tar -xf sky130A.tar.bz2
+# >>>>>>>>> THIS WORKS
+# Sky130 PDK
+echo "installing sky130A PDK"
+wget -q https://github.com/nayiri-k/hammer-workspace/raw/main/tech/sky130A.tar.bz2
+tar -xf sky130A.tar.bz2
 
-# # Sky130 SRAMs
-# echo "installing sky130 sram macros"
-# git clone https://github.com/efabless/sky130_sram_macros.git
+# Sky130 SRAMs
+echo "installing sky130 sram macros"
+git clone https://github.com/efabless/sky130_sram_macros.git
 
-# # Yosys
-# # conda create --name yosys --no-default-packages -y
-# echo "installing yosys"
-# # conda install -c timvideos -y yosys
-# wget -q https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2022-09-29/oss-cad-suite-linux-x64-20220929.tgz
-# tar zxf oss-cad-suite-linux-x64-20220929.tgz
-# export PATH=$TUTORIAL_INSTALL_PATH/oss-cad-suite/bin:$PATH
-# yosys -help
+# Yosys
+echo "installing yosys"
+wget -q https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2022-09-29/oss-cad-suite-linux-x64-20220929.tgz
+tar zxf oss-cad-suite-linux-x64-20220929.tgz
+export PATH=$TUTORIAL_INSTALL_PATH/oss-cad-suite/bin:$PATH
+yosys -help
+yosys -version
+which yosys
 
+# OpenROAD
+# first install dependencies
+echo "installing openroad dependencies"
+git clone --recursive https://github.com/The-OpenROAD-Project/OpenROAD.git
+cd OpenROAD
+sudo ./etc/DependencyInstaller.sh -dev
 
-# # OpenROAD
-# # first install dependencies
-# echo "installing openroad dependencies"
-# git clone --recursive https://github.com/The-OpenROAD-Project/OpenROAD.git
-# cd OpenROAD
-# sudo ./etc/DependencyInstaller.sh -dev
+echo "installing openroad"
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$TUTORIAL_INSTALL_PATH 
+make
+make install
 
-# echo "installing openroad"
-# mkdir build
-# cd build
-# cmake .. -DCMAKE_INSTALL_PREFIX=$TUTORIAL_INSTALL_PATH 
-# make
-# make install
-
-# openroad -help
-
-# # <<<<<<<<< THIS WORKS
-
+openroad -help
+openroad -version
+which openroad
 
 
-echo "end of tutorial setup testing"
+yosys -help
+openroad -help
+klayout -h
+
+yosys -version
+openroad -version
+klayout -v
+
+which yosys
+which openroad
+which klayout
+
+echo "End of tutorial setup testing"
+# <<<<<<<<< THIS WORKS
 
 
 
