1a5fee32467d125df5c5d8b8f42a36a199318ff7
Abraham Gonzalez <abe.j.gonza@gmail.com>
2022-10-01 08:16:22 -0500

    Update esp-install.sh

--- esp-install.sh
+++ esp-install.sh
@@ -2,16 +2,21 @@
 
 set -ex
 
+pushd /home/centos
 git clone https://github.com/ucb-bar/esp-isa-sim.git
-cd esp-isa-sim
+pushd esp-isa-sim
 git checkout 090e82c473fd28b4eb2011ffcd771ead6076faab
 
-cd build
+pushd build
 mkdir /home/centos/spike-local
 ../configure --prefix=/home/centos/spike-local --without-boost
 make –j16
 make install
 
 export PATH=/home/centos/spike-local:$PATH
+
+popd
+popd
+popd
 
 echo "Success! SHA3 Spike Built + Added to PATH"
