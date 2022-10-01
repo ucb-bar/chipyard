3c0ae43492ee338b228c830fde4f1745377738f0
Abraham Gonzalez <abe.j.gonza@gmail.com>
2022-10-01 11:50:05 -0500

    Update marshal-builds.sh

--- marshal-builds.sh
+++ marshal-builds.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pushd /home/centos/chipyard-afternoon/generators/sha3/software/
+marshal -v build br-base.json
 marshal -v build marshal-configs/sha3-linux.yaml
 marshal -v build marshal-configs/sha3-linux-jtr-crack.yaml
 marshal -v build marshal-configs/sha3-linux-jtr-test.yaml
