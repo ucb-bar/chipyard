777b1baabb557ecab30c392bfba3bb4baf4387f5
Abraham Gonzalez <abe.j.gonza@gmail.com>
2022-10-01 11:23:47 -0500

    Create marshal-builds.sh

---
+++ marshal-builds.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pushd /home/centos/chipyard-afternoon/generators/sha3/software/
+marshal -v build marshal-configs/sha3-linux.yaml
+marshal -v build marshal-configs/sha3-linux-jtr-crack.yaml
+marshal -v build marshal-configs/sha3-linux-jtr-test.yaml
+marshal -v install marshal-configs/sha3*.yaml
+popd
