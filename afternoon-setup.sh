88388b30bd510ff9783d5d8de52798def8f0fb57
Abraham Gonzalez <abe.j.gonza@gmail.com>
2022-10-01 11:43:05 -0500

    Update afternoon-setup.sh

--- afternoon-setup.sh
+++ afternoon-setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "export PATH=/home/centos/chipyard-afternoon/software/firemarshal:$PATH" >> ~/.bashrc
-echo "export PATH=/home/centos/spike-local/bin:$PATH" >> ~/.bashrc
+echo "export PATH=/home/centos/chipyard-afternoon/software/firemarshal:/home/centos/spike-local/bin:$PATH" >> ~/.bashrc
 
 echo "* soft nofile 16384" | sudo tee --append /etc/security/limits.conf
 echo "* hard nofile 16384" | sudo tee --append /etc/security/limits.conf
