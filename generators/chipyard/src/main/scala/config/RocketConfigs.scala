94494cc9664d7a14edc9c827b3581562110c6197
joey0320 <joonho0320@gmail.com>
2022-10-12 23:43:02 -0700

    Fix L2 cache config

--- generators/chipyard/src/main/scala/config/RocketConfigs.scala
+++ generators/chipyard/src/main/scala/config/RocketConfigs.scala
@@ -23,7 +23,8 @@ class TinyRocketConfig extends Config(
 
 class MempressConfig extends Config(
   new mempress.WithMemPress ++                                    // use Mempress (memory traffic generation) accelerator
-  new freechips.rocketchip.subsystem.WithInclusiveCache ++
+  new freechips.rocketchip.subsystem.WithNBanks(8) ++
+  new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=16, capacityKB=2048) ++
   new chipyard.config.WithExtMemIdBits(7) ++                      // use 7 bits for tl like request id
   new freechips.rocketchip.subsystem.WithNMemoryChannels(4) ++
   new chipyard.config.WithSystemBusWidth(128) ++
