95f30d0411645b12dfc5c0b14f9af0a5a678a991
Tushar Sondhi <tushar.sondhi@sifive.com>
2023-01-19 14:02:47 -0800

    add more minimal firesim configs for testing (#1313)

--- generators/firechip/src/main/scala/TargetConfigs.scala
+++ generators/firechip/src/main/scala/TargetConfigs.scala
@@ -139,6 +139,26 @@ class WithMinimalFireSimHighPerfConfigTweaks extends Config(
   new WithMinimalFireSimDesignTweaks
 )
 
+/**
+  * Adds BlockDevice to WithMinimalFireSimHighPerfConfigTweaks
+  */
+class WithMinimalAndBlockDeviceFireSimHighPerfConfigTweaks extends Config(
+  new WithFireSimHighPerfClocking ++
+  new freechips.rocketchip.subsystem.WithNoMemPort ++ // removes mem port for FASEDBridge to match against
+  new testchipip.WithBackingScratchpad ++ // adds backing scratchpad for memory to replace FASED model
+  new testchipip.WithBlockDevice(true) ++ // add in block device
+  new WithMinimalFireSimDesignTweaks
+)
+
+/**
+  *  Adds Block device to WithMinimalFireSimHighPerfConfigTweaks
+  */
+class WithMinimalAndFASEDFireSimHighPerfConfigTweaks extends Config(
+  new WithFireSimHighPerfClocking ++
+  new WithDefaultMemModel ++ // add default FASED memory model
+  new WithMinimalFireSimDesignTweaks
+)
+
 // Tweaks for legacy FireSim configs.
 class WithFireSimHighPerfConfigTweaks extends Config(
   new WithFireSimHighPerfClocking ++
