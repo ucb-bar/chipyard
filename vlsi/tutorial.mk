2bfc6e1347d656eeb4e7d8a28740a759e6b14433
Harrison Liew <harrisonliew@gmail.com>
2023-02-08 19:00:26 -0800

    [skip ci] abandon sv2v, Genus happy with patched firtool

--- vlsi/tutorial.mk
+++ vlsi/tutorial.mk
@@ -35,6 +35,4 @@ ifeq ($(tutorial),sky130-openroad)
     VLSI_OBJ_DIR      ?= build-sky130-openroad
     # This prevents multidimensional arrays (unsupported by Yosys) at the expense of elaboration time.
     #ENABLE_CUSTOM_FIRRTL_PASS = 1
-	# This runs sv2v for Yosys compatibility
-	CONVERT_SV2V = 1
 endif
