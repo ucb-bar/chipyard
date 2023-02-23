20587cfd40fbbb4ab57af851c3cef85f12fe2a9f
Tynan McAuley <tynan@niobiummicrosystems.com>
2023-02-22 22:05:31 -0800

    Run scalafmt after scala version update

--- src/main/scala/barstools/tapeout/transforms/ExtraTransforms.scala
+++ src/main/scala/barstools/tapeout/transforms/ExtraTransforms.scala
@@ -14,7 +14,7 @@ class ExtraLowTransforms extends Transform with DependencyAPIMigration {
   // this PropagatePresetAnnotations is needed to run the RemoveValidIf pass (that is removed from CIRCT).
   // additionally, since that pass isn't explicitly a prereq of the LowFormEmitter it
   // needs to wrapped in this xform
-  override def prerequisites:          Seq[TransformDependency] = Forms.LowForm :+
+  override def prerequisites: Seq[TransformDependency] = Forms.LowForm :+
     Dependency[firrtl.transforms.PropagatePresetAnnotations]
   override def optionalPrerequisites:  Seq[TransformDependency] = Forms.LowFormOptimized
   override def optionalPrerequisiteOf: Seq[TransformDependency] = Forms.LowEmitters
