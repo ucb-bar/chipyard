7c2ec8fb4c0c8a173fa207ebde29956b6c7360c6
Sagar Karandikar <sagark@eecs.berkeley.edu>
2023-03-09 01:48:25 +0000

    more fixes

--- xcelium.mk
+++ xcelium.mk
@@ -1,16 +1,7 @@
-HELP_COMPILATION_VARIABLES += \
-"   USE_VPD                = set to '1' to build VCS simulator to emit VPD instead of FSDB."
 
-HELP_SIMULATION_VARIABLES += \
-"   USE_VPD                = set to '1' to run VCS simulator emitting VPD instead of FSDB."
+WAVEFORM_FLAG=+vcdfile=$(sim_out_name).vcd
 
-ifndef USE_VPD
-WAVEFORM_FLAG=+fsdbfile=$(sim_out_name).fsdb
-else
-WAVEFORM_FLAG=+vcdplusfile=$(sim_out_name).vpd
-endif
-
-# If ntb_random_seed unspecified, vcs uses 1 as constant seed.
+# If ntb_random_seed unspecified, xcelium uses 1 as constant seed.
 # Set ntb_random_seed_automatic to actually get a random seed
 ifdef RANDOM_SEED
 SEED_FLAG=+ntb_random_seed=$(RANDOM_SEED)
@@ -32,19 +23,21 @@ XC_LD_PREFIX=-Wld,
 
 REMOVE_RPATH=-Wl,-rpath%
 
-VCS_CXXFLAGS = $(addprefix $(XC_CXX_PREFIX), $(SIM_CXXFLAGS))
-VCS_LDFLAGS = $(addprefix $(XC_LD_PREFIX), $(filter-out $(REMOVE_RPATH), $(SIM_LDFLAGS)))
+XCELIUM_CXXFLAGS = $(addprefix $(XC_CXX_PREFIX), $(SIM_CXXFLAGS))
+XCELIUM_LDFLAGS = $(addprefix $(XC_LD_PREFIX), $(filter-out $(REMOVE_RPATH), $(SIM_LDFLAGS)))
 
-# vcs requires LDFLAGS to not include library names (i.e. -l needs to be separate)
-VCS_CC_OPTS = \
-	$(VCS_CXXFLAGS) \
-	$(VCS_LDFLAGS) \
+XCELIUM_COMMON_ARGS = \
+	-64bit \
+	-xmlibdirname $(sim_workdir) \
+	-l /dev/null \
+	-log_xmsc_run /dev/null
+
+XCELIUM_CC_OPTS = \
+	$(XCELIUM_CXXFLAGS) \
+	$(XCELIUM_LDFLAGS) \
 	-enable_rpath
 
-#-LDFLAGS "$(filter-out -l%,$(VCS_LDFLAGS))" \
-#	$(filter -l%,$(VCS_LDFLAGS))
-
-VCS_NONCC_OPTS = \
+XCELIUM_NONCC_OPTS = \
 	-fast_recompilation \
 	-top $(TB) \
 	-sv \
@@ -54,14 +47,12 @@ VCS_NONCC_OPTS = \
 	-define INTC_NO_PWR_PINS \
 	-define INTC_EMULATION \
 	-f $(sim_common_files) \
-	-logfile xrun_elab.log \
 	-glsperf \
-	-genafile access.txt \
 	-notimingchecks \
 	-delay_mode zero
 
 PREPROC_DEFINES = \
-	-define VCS \
+	-define XCELIUM \
 	-define CLOCK_PERIOD=$(CLOCK_PERIOD) \
 	-define RESET_DELAY=$(RESET_DELAY) \
 	-define PRINTF_COND=$(TB).printf_cond \
@@ -72,6 +63,3 @@ PREPROC_DEFINES = \
 	-define RANDOMIZE_GARBAGE_ASSIGN \
 	-define RANDOMIZE_INVALID_ASSIGN
 
-ifndef USE_VPD
-PREPROC_DEFINES += +define+FSDB
-endif
