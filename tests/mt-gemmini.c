a7a132e379c463d23c45a3b9a3729228a8979a4c
Jerry Zhao <jerryz123@berkeley.edu>
2023-03-23 23:33:27 -0700

    Improve mt-gemmini

--- tests/mt-gemmini.c
+++ tests/mt-gemmini.c
@@ -2,29 +2,37 @@
 #include <stdio.h>
 #include "../generators/gemmini/software/gemmini-rocc-tests/include/gemmini.h"
 
-int main(void) {
-  __main();
-  return 0;
+const char* get_march(size_t marchid) {
+  switch (marchid) {
+  case 1:
+    return "rocket";
+  case 2:
+    return "sonicboom";
+  case 5:
+    return "spike";
+  default:
+    return "unknown";
+  }
 }
 
 volatile static int go_hart = 0;
 
 void __main(void) {
   size_t mhartid = read_csr(mhartid);
-  #define MAT_DIM_I 32
-  #define MAT_DIM_J 32
+  size_t marchid = read_csr(marchid);
+  const char* march = get_march(marchid);
+  #define MAT_DIM_I 16
+  #define MAT_DIM_J 16
   #define MAT_DIM_K 4
   #define NO_BIAS 1
   #define FULL_BIAS_WIDTH 1
-  
+
   static elem_t full_A[MAT_DIM_I][MAT_DIM_K] row_align(1);
   static elem_t full_B[MAT_DIM_K][MAT_DIM_J] row_align(1);
   static elem_t full_C[MAT_DIM_I][MAT_DIM_J] row_align(1);
   static acc_t full_D[MAT_DIM_I][MAT_DIM_J] row_align_acc(1);
-  
-  static elem_t gold[MAT_DIM_I][MAT_DIM_J];
-
   size_t start_cycle = rdcycle();
+  size_t start_insns = rdinstret();
 
   tiled_matmul_auto(MAT_DIM_I, MAT_DIM_J, MAT_DIM_K,
                     (elem_t*)full_A, (elem_t*)full_B, NO_BIAS ? NULL : &full_D[0][0], (elem_t*)full_C,
@@ -36,11 +44,40 @@ void __main(void) {
                     0,
                     WS);
   size_t end_cycle = rdcycle();
+  size_t end_insns = rdinstret();
+
+  size_t cpu_start_cycle = rdcycle();
+  size_t cpu_start_insns = rdinstret();
+
+  tiled_matmul_auto(MAT_DIM_I, MAT_DIM_J, MAT_DIM_K,
+                    (elem_t*)full_A, (elem_t*)full_B, NO_BIAS ? NULL : &full_D[0][0], (elem_t*)full_C,
+                    MAT_DIM_K, MAT_DIM_J, MAT_DIM_J, MAT_DIM_J,
+                    MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY, MVIN_SCALE_IDENTITY,
+                    NO_ACTIVATION, ACC_SCALE_IDENTITY, 0, false,
+                    false, false,
+                    false, !FULL_BIAS_WIDTH,
+                    0,
+                    CPU);
+  size_t cpu_end_cycle = rdcycle();
+  size_t cpu_end_insns = rdinstret();
+
 
   size_t cycles = end_cycle - start_cycle;
-
+  size_t cpu_cycles = cpu_end_cycle - cpu_start_cycle;
+  size_t insns = end_insns - start_insns;
+  size_t cpu_insns = cpu_end_insns - cpu_start_insns;
+  
   while (go_hart != mhartid);
-  printf("%d: %ld\n", mhartid, cycles);
+
+  printf("%s%d\n", march, mhartid);
+  printf(" G: %ld cyc %ld insns\n", cycles, insns);
+  printf(" C: %ld cyc %ld insns\n", cpu_cycles, cpu_insns);
   go_hart++;
   while (1);
+}
+
+
+int main(void) {
+  __main();
+  return 0;
 }
