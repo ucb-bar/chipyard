8e21af41d574a62e1d4653e3e62c14efd9bbf2f6
joey0320 <joonho0320@gmail.com>
2023-04-18 04:10:55 -0700

    more tweaks w/ diplomatic params|cleanup

--- generators/chipyard/src/main/scala/TileOnlyConfig.scala
+++ generators/chipyard/src/main/scala/TileOnlyConfig.scala
@@ -20,59 +20,49 @@ class TileOnlyTestHarness(implicit p: Parameters) extends Module {
 
   io.success := false.B
 
-  val lazyDut = LazyModule(p(BuildSystem)(p)).suggestName("TileOnlyDigitalTop")
+  val lazyDut = LazyModule(p(BuildSystem)(p)).suggestName("TileOnlyDigitalTop").asInstanceOf[TileOnlyDigitalTop]
   lazy val dut = Module(lazyDut.module)
 
 
-  dut match {
-    case dt :TileOnlyDigitalTopImp =>
-      println("Tie off the IOs")
+  val tlmaster = dut.outer.tileMasterIO.head
 
+  dontTouch(dut.outer.intSourceIO0(0))
+  dontTouch(dut.outer.intSourceIO1(0))
+  dontTouch(dut.outer.intSourceIO2(0))
+  dontTouch(dut.outer.intSourceIO3(0))
+  dontTouch(dut.outer.intSourceIO4(0))
+  dontTouch(dut.outer.hartIdSourceIO(0))
+  dontTouch(dut.outer.wfiSinkIO(0))
+  dontTouch(tlmaster)
 
-      val tlmaster = dt.outer.tileMasterIO.head
+  dut.outer.intSourceIO0(0).asUInt := 0.U
+  dut.outer.intSourceIO1(0).asUInt := 0.U
+  dut.outer.intSourceIO2(0).asUInt := 0.U
+  dut.outer.intSourceIO3(0).asUInt := 0.U
+  dut.outer.intSourceIO4(0).asUInt := 0.U
+  dut.outer.hartIdSourceIO(0) := 0.U
 
-
-      dontTouch(dt.outer.intSourceIO0(0))
-      dontTouch(dt.outer.intSourceIO1(0))
-      dontTouch(dt.outer.intSourceIO2(0))
-      dontTouch(dt.outer.intSourceIO3(0))
-      dontTouch(dt.outer.intSourceIO4(0))
-      dontTouch(dt.outer.hartIdSourceIO(0))
-      dontTouch(dt.outer.wfiSinkIO(0))
-      dontTouch(tlmaster)
-
-      dt.outer.intSourceIO0(0).asUInt := 0.U
-      dt.outer.intSourceIO1(0).asUInt := 0.U
-      dt.outer.intSourceIO2(0).asUInt := 0.U
-      dt.outer.intSourceIO3(0).asUInt := 0.U
-      dt.outer.intSourceIO4(0).asUInt := 0.U
-      dt.outer.hartIdSourceIO(0) := 0.U
-
-
-      tlmaster.a.ready := false.B
-      tlmaster.b.valid := false.B
-      tlmaster.b.bits.opcode := 0.U
-      tlmaster.b.bits.param := 0.U
-      tlmaster.b.bits.size := 0.U
-      tlmaster.b.bits.source := 0.U
-      tlmaster.b.bits.address := 0.U
-      tlmaster.b.bits.mask := 0.U
-      tlmaster.b.bits.data := 0.U
-      tlmaster.b.bits.corrupt := false.B
-      tlmaster.c.ready := false.B
-      tlmaster.d.valid := false.B
-      tlmaster.d.bits.opcode := 0.U
-      tlmaster.d.bits.param := 0.U
-      tlmaster.d.bits.size := 0.U
-      tlmaster.d.bits.source := 0.U
-      tlmaster.d.bits.sink := 0.U
-      tlmaster.d.bits.denied := false.B
-      tlmaster.d.bits.data := 0.U
-      tlmaster.d.bits.corrupt := false.B
-      tlmaster.e.ready := false.B
-
-    case _ =>
-  }
+  tlmaster.a.ready := false.B
+  tlmaster.b.valid := false.B
+  tlmaster.b.bits.opcode := 0.U
+  tlmaster.b.bits.param := 0.U
+  tlmaster.b.bits.size := 0.U
+  tlmaster.b.bits.source := 0.U
+  tlmaster.b.bits.address := 0.U
+  tlmaster.b.bits.mask := 0.U
+  tlmaster.b.bits.data := 0.U
+  tlmaster.b.bits.corrupt := false.B
+  tlmaster.c.ready := false.B
+  tlmaster.d.valid := false.B
+  tlmaster.d.bits.opcode := 0.U
+  tlmaster.d.bits.param := 0.U
+  tlmaster.d.bits.size := 0.U
+  tlmaster.d.bits.source := 0.U
+  tlmaster.d.bits.sink := 0.U
+  tlmaster.d.bits.denied := false.B
+  tlmaster.d.bits.data := 0.U
+  tlmaster.d.bits.corrupt := false.B
+  tlmaster.e.ready := false.B
 }
 
 class TileOnlyDigitalTop()(implicit p: Parameters)
@@ -81,9 +71,7 @@ class TileOnlyDigitalTop()(implicit p: Parameters)
 {
   println("Elaborating TileOnlyDigitalTop")
   println(s"p(RocketTileOnly ${p(RocketTileOnly)}")
-
   val tile = LazyModule(new RocketTile(p(RocketTileOnly), RocketCrossingParams(), PriorityMuxHartIdFromSeq(Seq(p(RocketTileOnly)))))
-
 
   ////////////////////////////////////////////////////////////////////////////
   // Diplomatic nodes required to punch out tile IOs
@@ -91,13 +79,13 @@ class TileOnlyDigitalTop()(implicit p: Parameters)
   val wfiSinkNode = IntSinkNode(IntSinkPortSimple())
   wfiSinkNode := tile.wfiNode
 
-  val hartIdSource = BundleBridgeSource(() => UInt(2.W))
+  val hartIdSource = BundleBridgeSource(() => UInt(1.W))
   tile.hartIdNode := hartIdSource
 
-  val beatBytes = 8
+  val beatBytes = p(XLen) / 8
   val masterNode = TLManagerNode(Seq(TLSlavePortParameters.v1(
     managers = Seq(TLManagerParameters(
-        address = Seq(AddressSet(0x20000, 0xfff)),
+        address = Seq(AddressSet(0x00000000, BigInt("FFFFFFFF", 16))),
         regionType = RegionType.CACHED,
         executable = true,
         supportsArithmetic = TransferSizes(1, beatBytes),
@@ -110,11 +98,11 @@ class TileOnlyDigitalTop()(implicit p: Parameters)
         supportsAcquireT = TransferSizes(64, 64),
         fifoId = Some(0))),
     beatBytes = beatBytes,
-    endSinkId = 1,
+    endSinkId = 7,
     minLatency = 1
   )))
 
-  masterNode :=* tile.masterNode
+  masterNode := tile.masterNode
 
   val intSourceNode0 = IntSourceNode(IntSourcePortSimple())
   val intSourceNode1 = IntSourceNode(IntSourcePortSimple())
@@ -126,7 +114,6 @@ class TileOnlyDigitalTop()(implicit p: Parameters)
   tile.intInwardNode := intSourceNode2
   tile.intInwardNode := intSourceNode3
   tile.intInwardNode := intSourceNode4
-
 
   ////////////////////////////////////////////////////////////////////////////
   // Placeholder nodes, will be optimized out by the firrtl compiler
@@ -143,13 +130,12 @@ class TileOnlyDigitalTop()(implicit p: Parameters)
 
   val haltSinkNode = IntSinkNode(IntSinkPortSimple())
   val ceaseSinkNode = IntSinkNode(IntSinkPortSimple())
-  val resetSource = BundleBridgeSource(() => UInt(18.W))
-  val nmiSource = BundleBridgeSource(() => new NMI(18))
+  val resetSource = BundleBridgeSource(() => UInt(32.W))
+  val nmiSource = BundleBridgeSource(() => new NMI(32))
   haltSinkNode := tile.haltNode
   ceaseSinkNode := tile.ceaseNode
   tile.resetVectorNode := resetSource
   tile.nmiNode := nmiSource
-
   ////////////////////////////////////////////////////////////////////////////
 
   val intSourceIO0      = InModuleBody { intSourceNode0.makeIOs() }
@@ -181,7 +167,7 @@ class WithRawRocketTileConfig extends Config((site, here, up) => {
   )
   case PgLevels => 3
   case XLen => 64
-  case MaxHartIdBits => 2
+  case MaxHartIdBits => 1
 })
 
 class TileOnlyRocketConfig extends Config(
