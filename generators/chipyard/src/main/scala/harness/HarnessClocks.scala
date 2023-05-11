1916d3e4fc5e794af6b293dfe1eca91314b6f09d
Jerry Zhao <jerryz123@berkeley.edu>
2023-05-10 21:30:59 -0700

    Add timeunit to ClockSourceAtFreqMHz

--- generators/chipyard/src/main/scala/harness/HarnessClocks.scala
+++ generators/chipyard/src/main/scala/harness/HarnessClocks.scala
@@ -48,7 +48,7 @@ class ClockSourceAtFreqMHz(val freqMHz: Double) extends BlackBox(Map(
       |    input power,
       |    input gate,
       |    output clk);
-      |
+      |  timeunit 1ns/1ps;
       |  reg clk_i = 1'b0;
       |  always #(PERIOD/2.0) clk_i = ~clk_i & (power & ~gate);
       |  assign clk = clk_i;
