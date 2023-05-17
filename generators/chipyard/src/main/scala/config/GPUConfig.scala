439a72f21bb60fcccd32a67fd73250e5ee8d1462
Vamber Yang <vamber@a8.Millennium.Berkeley.EDU>
2023-05-16 20:24:52 -0700

    Add new config for using PriorityXBar for coalescer

--- generators/chipyard/src/main/scala/config/GPUConfig.scala
+++ generators/chipyard/src/main/scala/config/GPUConfig.scala
@@ -55,3 +55,21 @@ class MemtraceCore256SbusConfig extends Config(
   new freechips.rocketchip.subsystem.WithNCustomSmallCores(1) ++
   new chipyard.config.AbstractConfig
 )
+
+class MemtraceCorePoXarConfig extends Config(
+  // Memtrace
+  new freechips.rocketchip.subsystem.WithMemtraceCore("vecadd.core1.thread4.trace",
+    traceHasSource = false) ++
+  // new freechips.rocketchip.subsystem.WithMemtraceCore("nvbit.vecadd.n100000.filter_sm0.trace",
+  //   traceHasSource = false) ++
+  new freechips.rocketchip.subsystem.WithCoalescer ++
+  new freechips.rocketchip.subsystem.WithPriorityCoalXbar++
+  new freechips.rocketchip.subsystem.WithNLanes(4) ++
+  // L2
+  new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=8, capacityKB=512) ++
+  new freechips.rocketchip.subsystem.WithNBanks(4) ++                                   
+  new chipyard.config.WithSystemBusWidth(64) ++ 
+  // Small Rocket core that does nothing
+  new freechips.rocketchip.subsystem.WithNCustomSmallCores(1) ++
+  new chipyard.config.AbstractConfig
+  )
