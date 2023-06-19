3806fedba3bdeb542bd8c0df0e4aa0f30f489106
Jerry Zhao <jerryz123@berkeley.edu>
2023-06-19 05:57:11 -0700

    Force specific locale

--- variables.mk
+++ variables.mk
@@ -304,3 +304,22 @@ GEN_COLLATERAL_DIR ?= $(build_dir)/gen-collateral
 #########################################################################################
 timeout_cycles = 10000000
 bmark_timeout_cycles = 100000000
+
+#########################################################################################
+# locale settings
+# some of the sbt/java commands will error on specific locales
+#########################################################################################
+export LANG=en_US.UTF-8
+export LC_CTYPE="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_PAPER="en_US.UTF-8"
+export LC_NAME="en_US.UTF-8"
+export LC_ADDRESS="en_US.UTF-8"
+export LC_TELEPHONE="en_US.UTF-8"
+export LC_MEASUREMENT="en_US.UTF-8"
+export LC_IDENTIFICATION="en_US.UTF-8"
+export LC_ALL=
