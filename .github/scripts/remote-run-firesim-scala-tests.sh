723e806af9de84876d2fab4daa781b2c23a6b5b1
abejgonzalez <abe.j.gonza@gmail.com>
2023-07-11 14:39:54 -0700

    Disable Vivado tests

--- .github/scripts/remote-run-firesim-scala-tests.sh
+++ .github/scripts/remote-run-firesim-scala-tests.sh
@@ -14,8 +14,9 @@ cd $REMOTE_CHIPYARD_DIR
 ./scripts/init-submodules-no-riscv-tools.sh --force
 
 # Run Firesim Scala Tests
-export FIRESIM_ENV_SOURCED=1;
+export FIRESIM_ENV_SOURCED=1
 export COURSIER_CACHE=$REMOTE_COURSIER_CACHE
 export JVM_MEMORY=10G
 export JAVA_TMP_DIR=$REMOTE_JAVA_TMP_DIR
+export TEST_DISABLE_VIVADO=1
 make -C $REMOTE_FIRESIM_DIR TARGET_SBT_PROJECT="{file:$REMOTE_CHIPYARD_DIR}firechip" testOnly ${mapping[$1]}
