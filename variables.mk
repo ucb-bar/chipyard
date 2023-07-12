e1b7588274e91624e1af4a3e634cc8e263f3837c
abejgonzalez <abe.j.gonza@gmail.com>
2023-07-12 14:01:39 -0700

    Simplified locale overrides

--- variables.mk
+++ variables.mk
@@ -309,17 +309,7 @@ bmark_timeout_cycles = 100000000
 # locale settings
 # some of the sbt/java commands will error on specific locales
 #########################################################################################
-export LANG=en_US.UTF-8
-export LC_CTYPE="en_US.UTF-8"
-export LC_NUMERIC="en_US.UTF-8"
-export LC_TIME="en_US.UTF-8"
-export LC_COLLATE="en_US.UTF-8"
-export LC_MONETARY="en_US.UTF-8"
-export LC_MESSAGES="en_US.UTF-8"
-export LC_PAPER="en_US.UTF-8"
-export LC_NAME="en_US.UTF-8"
-export LC_ADDRESS="en_US.UTF-8"
-export LC_TELEPHONE="en_US.UTF-8"
-export LC_MEASUREMENT="en_US.UTF-8"
-export LC_IDENTIFICATION="en_US.UTF-8"
-export LC_ALL=
+ENCODING="en_US.UTF-8"
+export LANG=$(ENCODING)
+export LANGUAGE="en_US:en"
+export LC_ALL=$(ENCODING)
