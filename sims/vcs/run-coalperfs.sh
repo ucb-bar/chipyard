d85a651324ceb1a9fea4a1217fe34382225ad8a9
Hansung Kim <hansung_kim@berkeley.edu>
2023-07-22 14:30:03 -0700

    run-coalperf.sh: disable 512-bit sbus configs
    Currently Chipyard fails to elaborate with SBus width of 512 bits. It's
    unclear if this is a problem in the coalescing unit or in Chipyard in
    general.  For now, only enable configs that use sbus widths up to 256
    bits.

--- sims/vcs/run-coalperfs.sh
+++ sims/vcs/run-coalperfs.sh
@@ -8,7 +8,9 @@ fi
 
 set -ex
 
-configurations=("MemtraceCoreNV64B8IdConfig" "MemtraceCoreNV128B8IdConfig" "MemtraceCoreNV256B8IdConfig" "MemtraceCoreNV512B8IdConfig" "MemtraceCoreNV64B16IdConfig" "MemtraceCoreNV128B16IdConfig" "MemtraceCoreNV256B16IdConfig" "MemtraceCoreNV512B16IdConfig" "MemtraceCoreNV64B32IdConfig" "MemtraceCoreNV128B32IdConfig" "MemtraceCoreNV256B32IdConfig" "MemtraceCoreNV512B32IdConfig") 
+configurations=("MemtraceCoreNV64B8IdConfig" "MemtraceCoreNV128B8IdConfig" "MemtraceCoreNV256B8IdConfig" "MemtraceCoreNV64B16IdConfig" "MemtraceCoreNV128B16IdConfig" "MemtraceCoreNV256B16IdConfig" "MemtraceCoreNV64B32IdConfig" "MemtraceCoreNV128B32IdConfig" "MemtraceCoreNV256B32IdConfig") 
+# Disabled as Chipyard fails to elaborate with 512b sbus:
+# "MemtraceCoreNV512B8IdConfig" "MemtraceCoreNV512B16IdConfig" "MemtraceCoreNV512B32IdConfig"
 
 rm -f coal_perf.txt
 # make clean
