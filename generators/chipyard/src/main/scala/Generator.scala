bee55315c4b0756a6f7d994187cad0a75fe78416
Abraham Gonzalez <abe.j.gonza@gmail.com>
2023-11-06 15:36:54 -0800

    Update Generator.scala

--- generators/chipyard/src/main/scala/Generator.scala
+++ generators/chipyard/src/main/scala/Generator.scala
@@ -1,6 +1,6 @@
 package chipyard
 
 import firrtl.options.{StageMain}
-import chipyard.stage.ChipyardStage
+import chipyard.stage.{ChipyardStage}
 
 object Generator extends StageMain(new ChipyardStage)
