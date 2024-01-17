0bb2a5c6f2e4ed4c545a85655a1e0f9f72fefce8
Hansung Kim <hansung_kim@berkeley.edu>
2024-01-16 16:31:26 -0800

    Accept EXTRA_SIM_PREPROC_DEFINES in run-radiance.sh

--- sims/vcs/run-radiance.sh
+++ sims/vcs/run-radiance.sh
@@ -2,12 +2,14 @@
 
 set -ex
 
-if [ $# -ne 2 ]; then
-    echo "usage: $0 config kernelname"
+if [ $# -lt 2 ]; then
+    echo "usage: $0 config kernelname EXTRA_SIM_PREPROC_DEFINES"
     exit 1
 fi
 CONFIG=$1
 KERNEL=$2
+[ $# -ge 3 ] && PREPROC_DEFINES=$3
+
 
 if ! diff ../args.bin ../args.bin.$KERNEL; then
     echo 'args.bin changed, running make clean'
@@ -20,4 +22,4 @@ ls -lh ../args.bin
 ls -lh ../op_a.bin
 ls -lh ../op_b.bin
 
-make run-binary-debug CONFIG=$CONFIG BINARY=/scratch/hansung/src/vortex2/tests/opencl/$KERNEL/$KERNEL.bin.elf LOADMEM=1
+make run-binary-debug CONFIG=$CONFIG BINARY=/scratch/hansung/src/vortex2/tests/opencl/$KERNEL/$KERNEL.bin.elf LOADMEM=1 EXTRA_SIM_PREPROC_DEFINES="$PREPROC_DEFINES"
