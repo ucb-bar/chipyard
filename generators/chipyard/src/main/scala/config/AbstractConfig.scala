4f3dd6067087b8bdeadc213bb4bf20320e53b5b9
Jerry Zhao <jerryz123@berkeley.edu>
2024-01-25 16:30:46 -0800

    Enable debug SBA on all default designs

--- generators/chipyard/src/main/scala/config/AbstractConfig.scala
+++ generators/chipyard/src/main/scala/config/AbstractConfig.scala
@@ -83,6 +83,7 @@ class AbstractConfig extends Config(
   new chipyard.config.WithInheritBusFrequencyAssignments ++         // Unspecified clocks within a bus will receive the bus frequency if set
   new freechips.rocketchip.subsystem.WithNMemoryChannels(1) ++      // Default 1 memory channels
   new freechips.rocketchip.subsystem.WithClockGateModel ++          // add default EICG_wrapper clock gate model
+  new freechips.rocketchip.subsystem.WithDebugSBA ++                // enable the SBA (system-bus-access) feature of the debug module
   new freechips.rocketchip.subsystem.WithJtagDTM ++                 // set the debug module to expose a JTAG port
   new freechips.rocketchip.subsystem.WithNoMMIOPort ++              // no top-level MMIO master port (overrides default set in rocketchip)
   new freechips.rocketchip.subsystem.WithNoSlavePort ++             // no top-level MMIO slave port (overrides default set in rocketchip)
