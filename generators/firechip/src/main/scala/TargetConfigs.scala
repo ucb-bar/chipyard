bf010668e3f2e53d8c40fa66301d820c78fd1e37
Jerry Zhao <jerryz123@berkeley.edu>
2024-01-26 18:40:08 -0800

    Bump firechip

--- generators/firechip/src/main/scala/TargetConfigs.scala
+++ generators/firechip/src/main/scala/TargetConfigs.scala
@@ -265,7 +265,7 @@ class FireSimSmallSystemConfig extends Config(
   new freechips.rocketchip.subsystem.WithExtMemSize(1 << 28) ++
   new testchipip.serdes.WithSerialTL(Seq(testchipip.serdes.SerialTLParams(
     client = Some(testchipip.serdes.SerialTLClientParams(idBits = 4)),
-    phyParams = testchipip.serdes.ExternalSyncSerialParams(width=32)
+    phyParams = testchipip.serdes.ExternalSyncSerialPhyParams(phitWidth=32, flitWidth=32)
   ))) ++
   new testchipip.iceblk.WithBlockDevice ++
   new chipyard.config.WithUARTInitBaudRate(BigInt(3686400L)) ++
