20496625294d569be9a7201b33df03e6fdfe4bf8
Mihai Tudor <mihai.tudor@berkeley.edu>
2024-04-30 11:41:13 -0700

    Added CharCount Config

--- generators/chipyard/src/main/scala/config/SpikeConfigs.scala
+++ generators/chipyard/src/main/scala/config/SpikeConfigs.scala
@@ -16,6 +16,11 @@ class SpikeAdderExampleConfig extends Config(
   new chipyard.WithNSpikeCores(1) ++
   new chipyard.config.AbstractConfig)
 
+class SpikeCharCountExampleConfig extends Config(
+  new chipyard.WithCharCountRoCC ++
+  new chipyard.WithNSpikeCores(1) ++
+  new chipyard.config.AbstractConfig)
+
 class SpikeEE290L1BMMRoCCConfig extends Config(
   new ee290.WithEE290RoCCAccelWithCacheBlackBox ++
   new chipyard.WithNSpikeCores(1) ++
