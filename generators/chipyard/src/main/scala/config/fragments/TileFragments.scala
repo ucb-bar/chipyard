c8d9299c70b50db82b081c29d6b4d5e3bf81a8b0
joonho hwangbo <joonho.whangbo@berkeley.edu>
2024-05-06 19:52:32 -0700

    Update generators/chipyard/src/main/scala/config/fragments/TileFragments.scala
    Co-authored-by: Tynan McAuley <tynan@niobiummicrosystems.com>

--- generators/chipyard/src/main/scala/config/fragments/TileFragments.scala
+++ generators/chipyard/src/main/scala/config/fragments/TileFragments.scala
@@ -132,7 +132,7 @@ class WithSV48IfPossible extends Config((site, here, up) => {
   case PgLevels => if (site(XLen) == 64) 4 /* Sv48 */ else up(PgLevels)
 })
 
-// Uses SV439 if possible, otherwise default to the Rocket Chip core default
+// Uses SV39 if possible, otherwise default to the Rocket Chip core default
 class WithSV39 extends Config((site, here, up) => {
   case PgLevels => { require(site(XLen) == 64); 3; }
 })
