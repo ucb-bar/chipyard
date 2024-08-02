e84dbd2700aede4c2a05d3be76259892d5f6acf0
SeahK <kimsea8209@berkeley.edu>
2024-08-01 20:36:27 -0700

    make it compatible with latest chipyard

--- fpga/src/main/scala/nexysvideo/HarnessBinders.scala
+++ fpga/src/main/scala/nexysvideo/HarnessBinders.scala
@@ -3,13 +3,14 @@ package chipyard.fpga.nexysvideo
 
 import chisel3._
 
+import freechips.rocketchip.jtag.{JTAGIO}
 import freechips.rocketchip.subsystem.{PeripheryBusKey}
 import freechips.rocketchip.tilelink.{TLBundle}
 import freechips.rocketchip.util.{HeterogeneousBag}
 import freechips.rocketchip.diplomacy.{LazyRawModuleImp}
 import org.chipsalliance.diplomacy.nodes.{HeterogeneousBag}
 
-import sifive.blocks.devices.uart.{UARTPortIO, HasPeripheryUARTModuleImp, UARTParams}
+import sifive.blocks.devices.uart.{UARTPortIO, UARTParams}
 import sifive.blocks.devices.jtag.{JTAGPins, JTAGPinsFromPort}
 import sifive.blocks.devices.pinctrl.{BasePin}
 
@@ -22,6 +23,7 @@ import sifive.fpgashells.clocks._
 import chipyard._
 import chipyard.harness._
 import chipyard.iobinders._
+import testchipip.serdes._
 
 class WithNexysVideoUARTTSI(uartBaudRate: BigInt = 115200) extends HarnessBinder({
   case (th: HasHarnessInstantiators, port: UARTTSIPort, chipId: Int) => {
@@ -47,41 +49,52 @@ class WithNexysVideoDDRTL extends HarnessBinder({
 
 // Uses PMOD JA/JB
 class WithNexysVideoSerialTLToGPIO extends HarnessBinder({
-  case (th: HasHarnessInstantiators, port: SerialTLPort) => {
+  case (th: HasHarnessInstantiators, port: SerialTLPort, chipId: Int) => {
     val nexysTh = th.asInstanceOf[LazyRawModuleImp].wrapper.asInstanceOf[NexysVideoHarness]
-    val harnessIO = IO(port.io.cloneType).suggestName("serial_tl")
+    val harnessIO = IO(chiselTypeOf(port.io)).suggestName("serial_tl")
     harnessIO <> port.io
-    val clkIO = IOPin(harnessIO.clock)
-    val packagePinsWithPackageIOs = Seq(
-      ("AB22", clkIO),
-      ("AB21", IOPin(harnessIO.bits.out.valid)),
-      ("AB20", IOPin(harnessIO.bits.out.ready)),
-      ("AB18", IOPin(harnessIO.bits.in.valid)),
-      ("Y21", IOPin(harnessIO.bits.in.ready)),
-      ("AA21", IOPin(harnessIO.bits.out.bits, 0)),
-      ("AA20", IOPin(harnessIO.bits.out.bits, 1)),
-      ("AA18", IOPin(harnessIO.bits.out.bits, 2)),
-      ("V9", IOPin(harnessIO.bits.out.bits, 3)),
-      ("V8", IOPin(harnessIO.bits.in.bits, 0)),
-      ("V7", IOPin(harnessIO.bits.in.bits, 1)),
-      ("W7", IOPin(harnessIO.bits.in.bits, 2)),
-      ("W9", IOPin(harnessIO.bits.in.bits, 3))
-    )
-    packagePinsWithPackageIOs foreach { case (pin, io) => {
-      nexysTh.xdc.addPackagePin(io, pin)
-      nexysTh.xdc.addIOStandard(io, "LVCMOS33")
-    }}
+    
+    harnessIO match {
+      case io: DecoupledPhitIO => {
+        val clkIO = io match {
+          case io: InternalSyncPhitIO => IOPin(io.clock_out)
+          case io: ExternalSyncPhitIO => IOPin(io.clock_in)
+        }
+        val packagePinsWithPackageIOs = Seq(
+          ("AB22", clkIO),
+          ("AB21", IOPin(io.out.valid)),
+          ("AB20", IOPin(io.out.ready)),
+          ("AB18", IOPin(io.in.valid)),
+          ("Y21", IOPin(io.in.ready)),
+          ("AA21", IOPin(io.out.bits.phit, 0)),
+          ("AA20", IOPin(io.out.bits.phit, 1)),
+          ("AA18", IOPin(io.out.bits.phit, 2)),
+          ("V9", IOPin(io.out.bits.phit, 3)),
+          ("V8", IOPin(io.in.bits.phit, 0)),
+          ("V7", IOPin(io.in.bits.phit, 1)),
+          ("W7", IOPin(io.in.bits.phit, 2)),
+          ("W9", IOPin(io.in.bits.phit, 3))
+        )
+        packagePinsWithPackageIOs foreach { case (pin, io) => {
+          nexysTh.xdc.addPackagePin(io, pin)
+          nexysTh.xdc.addIOStandard(io, "LVCMOS33")
+        }}
 
-    // Don't add IOB to the clock, if its an input
-    if (DataMirror.directionOf(port.io.clock) == Direction.Input) {
-      packagePinsWithPackageIOs foreach { case (pin, io) => {
-        nexysTh.xdc.addIOB(io)
-      }}
+        // Don't add IOB to the clock, if its an input
+        io match {
+          case io: InternalSyncPhitIO => packagePinsWithPackageIOs foreach { case (pin, io) => {
+            nexysTh.xdc.addIOB(io)
+          }}
+          case io: ExternalSyncPhitIO => packagePinsWithPackageIOs.drop(1).foreach { case (pin, io) => {
+            nexysTh.xdc.addIOB(io)
+          }}
+        }
+
+        nexysTh.sdc.addClock("ser_tl_clock", clkIO, 100)
+        nexysTh.sdc.addGroup(pins = Seq(clkIO))
+        nexysTh.xdc.clockDedicatedRouteFalse(clkIO)
+      }
     }
-
-    nexysTh.sdc.addClock("ser_tl_clock", clkIO, 100)
-    nexysTh.sdc.addGroup(pins = Seq(clkIO))
-    nexysTh.xdc.clockDedicatedRouteFalse(clkIO)
   }
 })
 
