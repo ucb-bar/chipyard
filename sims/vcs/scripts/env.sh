8e841f3326a7a1256916e71fe3ac6a2b74fd980c
Richard Yan <yrh@berkeley.edu>
2025-01-30 21:28:24 -0800

    update env script

--- sims/vcs/scripts/env.sh
+++ sims/vcs/scripts/env.sh
@@ -1,6 +1,4 @@
+#!/bin/bash
 
-echo "!!!!!!!!!!!!!!!1!!!!!!!!!!!"
-echo "need to update kernels path"
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-export KERNELS_PATH="/scratch/yrh/vxsw/kernels"
+ENV_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export KERNELS_PATH="$(realpath ${ENV_SCRIPT_DIR}/../../../../virgo-kernels/kernels)"
