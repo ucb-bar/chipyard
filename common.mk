b5a46de48593119c62f85665cc3c4fe8c1226cc9
Richard Yan <yrh@berkeley.edu>
2025-02-06 22:56:48 -0800

    fix makefile

--- common.mk
+++ common.mk
@@ -236,7 +236,8 @@ $(TOP_SMEMS_CONF) $(MODEL_SMEMS_CONF) &:  $(MFC_SMEMS_CONF) $(MFC_MODEL_HRCHY_JS
 		--model-module-name $(MODEL) \
 		--out-dut-smems-conf $(TOP_SMEMS_CONF) \
 		--out-model-smems-conf $(MODEL_SMEMS_CONF)
-	cat $(base_dir)/vlsi/add.mems.conf >> $(TOP_SMEMS_CONF)
+#	for blackboxed SRAMs: define mem here and use generated module name in blackbox source
+	-[ -f $(base_dir)/vlsi/add.mem.conf ] && cat $(base_dir)/vlsi/add.mems.conf >> $(TOP_SMEMS_CONF)
 
 # This file is for simulation only. VLSI flows should replace this file with one containing hard SRAMs
 TOP_MACROCOMPILER_MODE ?= --mode synflops
