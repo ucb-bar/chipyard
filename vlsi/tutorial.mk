f09034044f6ef8928c7d368077368ff1dee5157f
Fra <francesco.angione@polito.it>
2025-09-08 10:45:32 -0400

    feat: adding nangate45 2 additional commercial and openroad flows

--- vlsi/tutorial.mk
+++ vlsi/tutorial.mk
@@ -42,3 +42,31 @@ ifeq ($(tutorial),sky130-openroad)
     # Yosys compatibility for CIRCT-generated Verilog
     ENABLE_YOSYS_FLOW  = 1
 endif
+
+ifeq ($(tutorial),nangate45-openroad)
+    tech_name         ?= nangate45
+    CONFIG            ?= TinyRocketConfig
+    TOOLS_CONF        ?= example-openroad.yml
+    TECH_CONF         ?= example-nangate45.yml
+    DESIGN_CONFS      ?= example-designs/nangate45-openroad.yml \
+                        $(if $(filter $(VLSI_TOP),Rocket), \
+                            example-designs/nangate45-rocket.yml) \
+                        $(if $(filter $(VLSI_TOP),RocketTile), \
+                            example-designs/nangate45-openroad-rockettile.yml, )
+    VLSI_OBJ_DIR      ?= build-nangate45-openroad
+    INPUT_CONFS       ?= $(TOOLS_CONF) $(TECH_CONF) $(DESIGN_CONFS) $(EXTRA_CONFS)
+    # Yosys compatibility for CIRCT-generated Verilog
+    ENABLE_YOSYS_FLOW  = 1
+endif
+
+ifeq ($(tutorial),nangate45-commercial)
+    tech_name         ?= nangate45
+    CONFIG            ?= RocketConfig
+    TOOLS_CONF        ?= example-tools.yml
+    TECH_CONF         ?= example-nangate45.yml
+    DESIGN_CONFS      ?= example-designs/nangate45-commercial.yml \
+                        $(if $(filter $(VLSI_TOP),Rocket), \
+                            example-designs/nangate45-rocket.yml, )
+    VLSI_OBJ_DIR      ?= build-nangate45-commercial
+    INPUT_CONFS       ?= $(TOOLS_CONF) $(TECH_CONF) $(DESIGN_CONFS) $(EXTRA_CONFS)
+endif
