1f1a2ae9ae040c35bff4437d9abc47d1c5d48db6
Lux <iansseijelly@berkeley.edu>
2025-12-02 15:28:55 -0800

    feat: switch to streaming engine interface

--- generators/firechip/bridgestubs/src/main/cc/bridges/tacit.cc
+++ generators/firechip/bridgestubs/src/main/cc/bridges/tacit.cc
@@ -8,6 +8,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <array>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
 
 // #define DEBUG
 
@@ -15,20 +19,70 @@ char tacit_t::KIND;
 
 class tacit_handler_impl final : public tacit_handler {
 public:
-  tacit_handler_impl(int tacitno, int tacitlogfd) {
-    this->tacitlogfd = tacitlogfd;
-  }
+  tacit_handler_impl(int tacitno, int tacitlogfd);
+  ~tacit_handler_impl() override;
+
 protected:
   int tacitlogfd;
+  static constexpr size_t BUFFER_SIZE = 4096;
+  std::array<uint8_t, BUFFER_SIZE> buffer{};
+  size_t buffered_bytes = 0;
 
   void put(uint8_t data) override;
+  void flush_buffer();
 };
 
+static void fatal_io_error(int fd, const char *operation) {
+  fprintf(stderr,
+          "TACIT log fd %d: failed to %s: %s\n",
+          fd,
+          operation,
+          strerror(errno));
+  abort();
+}
+
+tacit_handler_impl::tacit_handler_impl(int tacitno, int tacitlogfd) {
+  (void)tacitno;
+  this->tacitlogfd = tacitlogfd;
+}
+
+tacit_handler_impl::~tacit_handler_impl() {
+  if (buffered_bytes) {
+    flush_buffer();
+  }
+  while (::close(tacitlogfd) == -1) {
+    if (errno == EINTR) {
+      continue;
+    }
+    fatal_io_error(tacitlogfd, "close");
+  }
+}
+
 void tacit_handler_impl::put(uint8_t data) {
-  write(tacitlogfd, &data, 1);
+  buffer[buffered_bytes++] = data;
+  if (buffered_bytes == BUFFER_SIZE) {
+    flush_buffer();
+  }
   #ifdef DEBUG
     fprintf(stderr, "TACIT%d: %02x\n", tacitlogfd, data);
   #endif
+}
+
+void tacit_handler_impl::flush_buffer() {
+  size_t offset = 0;
+  while (offset < buffered_bytes) {
+    ssize_t bytes_written =
+        ::write(tacitlogfd, buffer.data() + offset, buffered_bytes - offset);
+    if (bytes_written > 0) {
+      offset += static_cast<size_t>(bytes_written);
+      continue;
+    }
+    if (bytes_written == -1 && errno == EINTR) {
+      continue;
+    }
+    fatal_io_error(tacitlogfd, "write");
+  }
+  buffered_bytes = 0;
 }
 
 static std::unique_ptr<tacit_handler>
@@ -45,36 +99,54 @@ create_handler(const std::vector<std::string> &args, int tacitno) {
 }
 
 tacit_t::tacit_t(simif_t &simif,
-                 const TACITBRIDGEMODULE_struct &mmio_addrs,
+                 StreamEngine &stream,
                  int tacitno,
-                 const std::vector<std::string> &args)
-    : bridge_driver_t(simif, &KIND), mmio_addrs(mmio_addrs),
-      handler(create_handler(args, tacitno)) {}
+                 const std::vector<std::string> &args,
+                 int stream_idx,
+                 int stream_depth)
+    : streaming_bridge_driver_t(simif, stream, &KIND),
+      handler(create_handler(args, tacitno)),
+      stream_idx(stream_idx),
+      stream_depth(stream_depth) {}
 
 tacit_t::~tacit_t() = default;
 
-void tacit_t::recv() {
-  data.out.valid = read(mmio_addrs.out_valid);
-  if (data.out.valid) {
-    data.out.bits = read(mmio_addrs.out_bits);
-  }
-}
-
-void tacit_t::send() {
-  if (data.out.fire()) {
-    write(mmio_addrs.out_ready, data.out.ready);
-  }
-}
-
 void tacit_t::tick() {
-  data.out.ready = true;
-  do {
-    this->recv();
-    // data serves as an intermediate buffer 
-    // between the bridge and the handler
-    if (data.out.fire()) {
-      handler->put(data.out.bits);
+  drain_stream(STREAM_WIDTH_BYTES);
+}
+
+void tacit_t::finish() {
+  pull_flush(stream_idx);
+  drain_stream(0);
+  handler->flush_buffer();
+}
+
+void tacit_t::drain_stream(size_t minimum_batch_bytes) {
+  const size_t max_batch_bytes =
+      static_cast<size_t>(stream_depth) * STREAM_WIDTH_BYTES;
+  if (!handler || stream_depth == 0 || max_batch_bytes == 0) {
+    return;
+  }
+
+  page_aligned_sized_array(outbuf, STREAM_WIDTH_BYTES * stream_depth);
+
+  size_t min_bytes = minimum_batch_bytes;
+  while (true) {
+    const size_t bytes_received =
+        pull(stream_idx, outbuf, max_batch_bytes, min_bytes);
+    if (bytes_received == 0) {
+      break;
     }
-    this->send();
-  } while (data.out.fire());
+
+    auto *byte_buf = reinterpret_cast<uint8_t *>(outbuf);
+    for (size_t i = 0; i < bytes_received; ++i) {
+      handler->put(byte_buf[i]);
+    }
+
+    if (bytes_received < max_batch_bytes && min_bytes == 0) {
+      break;
+    }
+
+    min_bytes = 0;
+  }
 }
--- generators/firechip/bridgestubs/src/main/cc/bridges/tacit.h
+++ generators/firechip/bridgestubs/src/main/cc/bridges/tacit.h
@@ -3,7 +3,6 @@
 #ifndef __TACITBRIDGE_H
 #define __TACITBRIDGE_H
 
-#include "bridges/serial_data.h"
 #include "core/bridge_driver.h"
 
 #include <cstdint>
@@ -13,6 +12,8 @@
 #include <string>
 #include <vector>
 
+class StreamEngine;
+
 /**
  * Structure carrying the addresses of all fixed MMIO ports.
  *
@@ -20,40 +21,40 @@
  * the target configuration.
  */
 struct TACITBRIDGEMODULE_struct {
-  uint64_t out_bits;
-  uint64_t out_valid;
-  uint64_t out_ready;
 };
 
 class tacit_handler {
 public:
   virtual ~tacit_handler() = default;
   virtual void put(uint8_t data) = 0;
+  virtual void flush_buffer() = 0;
 };
 
-class tacit_t final : public bridge_driver_t {
+class tacit_t final : public streaming_bridge_driver_t {
 public:
   /// The identifier for the bridge type used for casts.
   static char KIND;
 
   /// Creates a bridge which interacts with standard streams or PTY.
   tacit_t(simif_t &simif,
-         const TACITBRIDGEMODULE_struct &mmio_addrs,
-         int tacitno,
-         const std::vector<std::string> &args);
+          StreamEngine &stream,
+          int tacitno,
+          const std::vector<std::string> &args,
+          int stream_idx,
+          int stream_depth);
 
   ~tacit_t() override;
 
   void tick() override;
+  void finish() override;
 
 private:
-  const TACITBRIDGEMODULE_struct mmio_addrs;
   std::unique_ptr<tacit_handler> handler;
 
-  serial_data_t<uint8_t> data;
+  const int stream_idx;
+  const int stream_depth;
 
-  void recv();
-  void send();
+  void drain_stream(size_t minimum_batch_bytes);
 };
 
 #endif // __TACITBRIDGE_H
